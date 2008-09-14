b246607728bd70c35816004b9313d87d20c1b6df
Mike Dalessio <mike@csa.net>
2008-09-14 18:45:35 -0400

    added nokogiri/hpricot.rb which contains aaron's convert_to_xpath method. this is monkeypatched into XML::Node. removed previous kludge for xpath munging.

--- lib/nokogiri/xml/xpath.rb
+++ lib/nokogiri/xml/xpath.rb
@@ -1,11 +1,6 @@
 module Nokogiri
   module XML
     class XPath
-      class << self
-        def munge_search_path(search_path)
-          search_path.gsub(%r{^(//.*)},'.\1')
-        end
-      end
     end
   end
 end
