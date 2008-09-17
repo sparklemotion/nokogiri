66916bfde98c704b9bc3674d8f20c45b25c111e3
Aaron Patterson <aaron.patterson@gmail.com>
2008-09-17 08:54:18 -0700

    refactoring decorate code

--- lib/nokogiri/hpricot.rb
+++ lib/nokogiri/hpricot.rb
@@ -5,15 +5,15 @@ module Nokogiri
     class << self
       def parse(*args)
         doc = Nokogiri.parse(*args)
-        doc.extend(Decorators::Hpricot)
-        doc.decorators['node'] << Decorators::Hpricot
-        doc.decorators['document'] << Decorators::Hpricot
-        doc.decorate!
-        doc
+        add_decorators(doc)
       end
 
       def XML(string)
         doc = Nokogiri::XML.parse(string)
+        add_decorators(doc)
+      end
+
+      def add_decorators(doc)
         doc.decorators['node'] << Decorators::Hpricot
         doc.decorators['document'] << Decorators::Hpricot
         doc.decorate!
@@ -26,19 +26,11 @@ module Nokogiri
     def Hpricot(*args, &block)
       if block_given?
         builder = Nokogiri::HTML::Builder.new(&block)
-        doc = builder.doc
-        doc.decorators['node'] << Decorators::Hpricot
-        doc.decorators['document'] << Decorators::Hpricot
-        doc.decorate!
-        return doc
+        Nokogiri::Hpricot.add_decorators(builder.doc)
       else
         doc = Nokogiri::HTML.parse(*args)
-        doc.decorators['node'] << Decorators::Hpricot
-        doc.decorators['document'] << Decorators::Hpricot
-        doc.decorate!
-        doc
+        Nokogiri::Hpricot.add_decorators(doc)
       end
     end
   end
-
 end
