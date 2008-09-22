18f7047add193dd98cadb59db45a268623fcdf8e
John Barnette <jbarnette@gmail.com>
2008-09-22 12:09:35 -0700

    Building the extension will fail helpfully if frex and racc aren't available.

--- ext/nokogiri/extconf.rb
+++ ext/nokogiri/extconf.rb
@@ -8,13 +8,17 @@ $CFLAGS << " -O3 -Wall -Wextra -Wcast-qual -Wwrite-strings -Wconversion -Wmissin
 find_library('xml2', 'xmlParseDoc')
 find_library('xslt', 'xsltParseStylesheetDoc')
 
-unless find_header('libxml/xmlversion.h', '/usr/include/libxml2')
-  puts "need libxml"
-  exit 1
+unless find_header('libxml/xmlversion.h', '/usr/include/libxml2') &&
+    find_header('libxslt/xslt.h', '/usr/include')
+  abort "need libxml"
 end
-unless find_header('libxslt/xslt.h', '/usr/include')
-  puts "need libxml"
-  exit 1
+
+unless find_executable("racc")
+  abort "need racc, get the tarball from http://i.loveruby.net/archive/racc/racc-1.4.5-all.tar.gz" 
+end
+
+unless find_executable("frex")
+  abort "need frex, sudo gem install aaronp-frex -s http://gems.github.com"   
 end
 
 create_makefile('nokogiri/native')
