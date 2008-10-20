10ffcc4ee665b7fcaab1a933808f640aa4122314
Mike Dalessio <mike@csa.net>
2008-10-20 14:43:56 -0400

    working around unsafe memory operation in node set GC.

--- ext/nokogiri/xml_node_set.c
+++ ext/nokogiri/xml_node_set.c
@@ -66,8 +66,40 @@ static void gc_mark(xmlNodeSetPtr node_set)
 
 static void deallocate(xmlNodeSetPtr node_set)
 {
+  /*
+   *  xmlXPathFreeNodeSet() contains an implicit assumption that it is being
+   *  called before any of its pointed-to nodes have been free()d. this
+   *  assumption lies in the operation where it dereferences nodeTab pointers
+   *  while searching for namespace nodes to free.
+   *
+   *  however, since Ruby's GC mechanism cannot guarantee the strict order in
+   *  which ruby objects will be GC'd, nodes may be garbage collected before a
+   *  nodeset containing pointers to those nodes. (this is true regardless of
+   *  how we declare dependencies between objects with rb_gc_mark().)
+   *
+   *  as a result, xmlXPathFreeNodeSet() will perform unsafe memory operations,
+   *  and calling it would be evil.
+   *
+   *  on the bright side, though, Nokogiri's API currently does not cause
+   *  namespace nodes to be included in node sets, ever.
+   *
+   *  armed with that fact, we examined xmlXPathFreeNodeSet() and related libxml
+   *  code and determined that, within the Nokogiri abstraction, we will not
+   *  leak memory if we simply free the node set's memory directly. that's only
+   *  quasi-evil!
+   *
+   *  there's probably a lesson in here somewhere about intermingling, within a
+   *  single array, structs with different memory-ownership semantics. or more
+   *  generally, a lesson about building an API in C/C++ that does not contain
+   *  assumptions about the strict order in which memory will be released. hey,
+   *  that sounds like a great idea for a blog post! get to it!
+   *
+   *  "In Valgrind We Trust." seriously.
+   */
   NOKOGIRI_DEBUG_START(node_set) ;
-  xmlXPathFreeNodeSet(node_set);
+  if (node_set->nodeTab != NULL)
+    xmlFree(node_set->nodeTab);
+  xmlFree(node_set);
   NOKOGIRI_DEBUG_END(node_set) ;
 }
 
