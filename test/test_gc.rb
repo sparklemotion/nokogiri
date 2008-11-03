4ca417f710a73814261e2c7e806612f6d3b0d157
Mike Dalessio <mike@csa.net>
2008-11-03 14:40:47 -0500

    new tests for GC bug

---
+++ test/test_gc.rb
@@ -0,0 +1,15 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
+
+class TestGc < Nokogiri::TestCase
+
+  def test_dont_hurt_em_why
+    content = File.open("#{File.dirname(__FILE__)}/files/dont_hurt_em_why.xml")
+    ndoc = Nokogiri::XML(content)
+    2.times do
+      info = ndoc.search('status text').first.inner_text
+      url = ndoc.search('user name').first.inner_text
+      GC.start
+    end
+  end
+
+end
