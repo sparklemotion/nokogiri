f1b2c5434702b7d319b9a207a3bf2baa73823dd7
Aaron Patterson <aaron.patterson@gmail.com>
2008-11-11 08:57:56 -0800

    defaulting ENV["PATH"] on windows if it does not exist.  annoying....

--- lib/nokogiri.rb
+++ lib/nokogiri.rb
@@ -8,9 +8,9 @@ require 'nokogiri/html/builder'
 require 'nokogiri/hpricot'
 
 # Modify the PATH on windows so that the external DLLs will get loaded.
-ENV['PATH'] += ";" + File.expand_path(
+ENV['PATH'] = [ENV['PATH'], File.expand_path(
   File.join(File.dirname(__FILE__), "..", "ext", "nokogiri")
-) if RUBY_PLATFORM =~ /mswin/i
+)].compact.join(';') if RUBY_PLATFORM =~ /mswin/i
 
 require 'nokogiri/native' unless RUBY_PLATFORM =~ /java/
 
