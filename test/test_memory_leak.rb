0be1065d1d0b94abf49a49fc7b459b8a9f62e655
Mike Dalessio <mike@csa.net>
2008-11-17 02:31:10 -0500

    added test to detect memory leak

---
+++ test/test_memory_leak.rb
@@ -0,0 +1,38 @@
+require File.expand_path(File.join(File.dirname(__FILE__), "helper"))
+
+class TestMemoryLeak < Nokogiri::TestCase
+  def test_for_memory_leak
+    begin
+      #  we don't use Dike in any tests, but requiring it has side effects
+      #  that can create memory leaks, and that's what we're testing for.
+      require 'rubygems'
+      require 'dike' # do not remove!
+
+      count_start = count_object_space_documents
+      xml_data = <<-EOS
+        <test>
+          <items>
+            <item>abc</item>
+            <item>1234</item>
+            <item>Zzz</item>
+          <items>
+        </test>
+        EOS
+      10.times do
+        doc = Nokogiri::XML(xml_data)
+        doc.xpath("//item")
+      end
+      GC.start
+      count_end = count_object_space_documents
+      assert((count_end - count_start) <= 1, "memory leak detected")
+    rescue LoadError
+      puts "\ndike is not installed, skipping memory leak test"
+    end
+  end
+
+  def count_object_space_documents
+    count = 0
+    ObjectSpace.each_object {|j| count += 1 if j.is_a?(Nokogiri::XML::Document) }
+    count
+  end
+end
