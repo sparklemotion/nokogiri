caba25a8fc363ffdc9b0b66a8cd2548cc62e7c46
Aaron Patterson <aaron.patterson@gmail.com>
2009-01-28 22:33:04 -0800

    adding more push parsing tests

--- test/xml/sax/test_push_parser.rb
+++ test/xml/sax/test_push_parser.rb
@@ -20,6 +20,23 @@ module Nokogiri
           assert @parser.document.end_document_called
         end
 
+        def test_start_element
+          @parser.<<(<<-eoxml)
+            <p id="asdfasdf">
+          eoxml
+
+          assert_equal [["p", ["id", "asdfasdf"]]],
+            @parser.document.start_elements
+
+          @parser.<<(<<-eoxml)
+              <!-- This is a comment -->
+              Paragraph 1
+            </p>
+          eoxml
+          assert_equal [' This is a comment '], @parser.document.comments
+          @parser.finish
+        end
+
         def test_chevron_partial_xml
           @parser.<<(<<-eoxml)
             <p id="asdfasdf">
