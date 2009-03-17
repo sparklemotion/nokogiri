3c50766d4226a97c0b1de9db1d67df8cb3bf710e
Aaron Patterson <aaron.patterson@gmail.com>
2009-03-17 10:17:23 -0700

    fixing bug where node is passed in to Node#new and caching ruby pointer in node->_private

--- ext/nokogiri/xml_node.h
+++ ext/nokogiri/xml_node.h
@@ -7,6 +7,8 @@ void init_xml_node();
 VALUE Nokogiri_wrap_xml_node(xmlNodePtr root);
 
 extern VALUE cNokogiriXmlNode ;
+extern VALUE cNokogiriXmlElement ;
+
 VALUE Nokogiri_wrap_xml_node(xmlNodePtr node) ;
 void Nokogiri_xml_node_properties(xmlNodePtr node, VALUE attr_hash) ;
 void Nokogiri_xml_node_namespaces(xmlNodePtr node, VALUE attr_hash) ;
