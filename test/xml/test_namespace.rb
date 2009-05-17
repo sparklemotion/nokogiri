409a31400b09efe36ed300638ca90be04aef68eb
Aaron Patterson <aaron.patterson@gmail.com>
2009-05-16 23:06:38 -0700

    test refactoring

--- test/xml/test_namespace.rb
+++ test/xml/test_namespace.rb
@@ -3,29 +3,30 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', "helper"))
 module Nokogiri
   module XML
     class TestNamespace < Nokogiri::TestCase
-      def test_namespace_node_prefix
-        xml = Nokogiri::XML <<-eoxml
+      def setup
+        @xml = Nokogiri::XML <<-eoxml
           <root xmlns="http://tenderlovemaking.com/" xmlns:foo="bar">
             <awesome/>
           </root>
         eoxml
-        awesome = xml.root
-        namespaces = awesome.namespace_definitions
+      end
+
+      def test_namespace_node_prefix
+        namespaces = @xml.root.namespace_definitions
         assert_equal [nil, 'foo'], namespaces.map { |x| x.prefix }
       end
 
       def test_namespace_node_href
-        xml = Nokogiri::XML <<-eoxml
-          <root xmlns="http://tenderlovemaking.com/" xmlns:foo="bar">
-            <awesome/>
-          </root>
-        eoxml
-        awesome = xml.root
-        namespaces = awesome.namespace_definitions
+        namespaces = @xml.root.namespace_definitions
         assert_equal [
           'http://tenderlovemaking.com/',
           'bar'
         ], namespaces.map { |x| x.href }
+      end
+
+      def test_equality
+        namespaces = @xml.root.namespace_definitions
+        assert_equal namespaces, @xml.root.namespace_definitions
       end
     end
   end
