bb6252a0ef69ef41d32b5349e80b46883089905d
Aaron Patterson <aaron.patterson@gmail.com>
2009-05-16 19:22:37 -0700

    test refactor

--- test/xml/test_node.rb
+++ test/xml/test_node.rb
@@ -28,18 +28,7 @@ module Nokogiri
             assert_instance_of klass, node
           end
         }
-      end
 
-      {
-        Nokogiri::XML::CDATA                  => 'doc, "foo"',
-        Nokogiri::XML::Attr                   => 'doc, "foo"',
-        Nokogiri::XML::Comment                => 'doc, "foo"',
-        Nokogiri::XML::EntityReference        => 'doc, "foo"',
-        Nokogiri::XML::ProcessingInstruction  => 'doc, "foo", "bar"',
-        Nokogiri::XML::DocumentFragment       => 'doc',
-        Nokogiri::XML::Node                   => '"foo", doc',
-        Nokogiri::XML::Text                   => '"foo", doc',
-      }.each do |klass, constructor|
         class_eval <<-eocode, __FILE__, __LINE__ + 1
           def test_subclass_initialize_#{klass.name.gsub('::', '_')}
             doc = Nokogiri::XML::Document.new
