0eeac8724f17b37605ed9f325eb9ae4c89346d10
Sergio Arbeo <sergio@Ystad.(none)>
2009-06-17 21:30:35 +0200

    Added a test

--- test/test_jruby.rb
+++ test/test_jruby.rb
@@ -3,6 +3,7 @@ ROOT_DIR = File.dirname(__FILE__)
 XML_DIR = File.join(ROOT_DIR,'xml')
 load File.join(ROOT_DIR, 'test_reader.rb')
 load File.join(XML_DIR, 'test_namespace.rb')
+load File.join(XML_DIR, 'test_node.rb')
 load File.join(XML_DIR, 'test_node_attributes.rb')
 
 #suite = TestSuite.new "JRuby test"
