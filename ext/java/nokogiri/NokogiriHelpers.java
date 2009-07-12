089f143410a0ae96f53870b4c21fce9847ae7b3d
Sergio Arbeo <sergio@Ystad.(none)>
2009-07-12 16:05:36 +0200

    Added path method.

--- ext/java/nokogiri/NokogiriHelpers.java
+++ ext/java/nokogiri/NokogiriHelpers.java
@@ -5,6 +5,7 @@
 
 package nokogiri;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
@@ -24,17 +25,214 @@ public class NokogiriHelpers {
     }
 
     public static String getNodeCompletePath(Node node) {
-        return getNodeCompletePath(node,"");
+
+        Node cur, tmp, next;
+
+        // TODO: Rename buffer to path.
+        String buffer = "";
+        String sep;
+        String name;
+
+        int occur = 0, generic;
+
+        cur = node;
+
+        do {
+            name = "";
+            sep = "?";
+            occur = 0;
+            generic = 0;
+
+            if(cur.getNodeType() == Node.DOCUMENT_NODE) {
+                if(buffer.startsWith("/")) break;
+
+                sep = "/";
+                next = null;
+            } else if(cur.getNodeType() == Node.ELEMENT_NODE) {
+                generic = 0;
+                sep = "/";
+
+                name = cur.getLocalName();
+                if(cur.getNamespaceURI() != null) {
+                    if(cur.getPrefix() != null) {
+                        name = cur.getPrefix() + ":" + name;
+                    } else {
+                        generic = 1;
+                        name = "*";
+                    }
+                }
+
+                next = cur.getParentNode();
+
+                /*
+                 * Thumbler index computation
+                 */
+
+                tmp = cur.getPreviousSibling();
+
+                while(tmp != null) {
+                    if((tmp.getNodeType() == Node.ELEMENT_NODE) &&
+                        (generic != 0 || compareTwoNodes(tmp,cur))) {
+                        occur++;
+                    }
+                    tmp = tmp.getPreviousSibling();
+                }
+
+                if(occur == 0) {
+                    tmp = cur.getNextSibling();
+
+                    while(tmp != null && occur == 0) {
+                        if((tmp.getNodeType() == Node.ELEMENT_NODE) &&
+                            (generic != 0 || compareTwoNodes(tmp,cur))) {
+                            occur++;
+                        }
+                        tmp = tmp.getNextSibling();
+                    }
+
+                    if(occur != 0) occur = 1;
+
+                } else {
+                    occur++;
+                }
+            } else if(cur.getNodeType() == Node.COMMENT_NODE) {
+                sep = "/";
+                name = "comment()";
+                next = cur.getParentNode();
+
+                /*
+                 * Thumbler index computation.
+                 */
+
+                tmp = cur.getPreviousSibling();
+
+                while(tmp != null) {
+                    if(tmp.getNodeType() == Node.COMMENT_NODE) {
+                        occur++;
+                    }
+                    tmp = tmp.getPreviousSibling();
+                }
+
+                if(occur == 0) {
+                    tmp = cur.getNextSibling();
+                    while(tmp != null && occur == 0) {
+                        if(tmp.getNodeType() == Node.COMMENT_NODE) {
+                            occur++;
+                        }
+                        tmp = tmp.getNextSibling();
+                    }
+                    if(occur != 0) occur = 1;
+                } else {
+                    occur = 1;
+                }
+
+            } else if(cur.getNodeType() == Node.TEXT_NODE ||
+                cur.getNodeType() == Node.CDATA_SECTION_NODE) {
+                    // I'm here. gist:129
+                    // http://gist.github.com/144923
+
+                sep = "/";
+                name = "text()";
+                next = cur.getParentNode();
+
+                /*
+                 * Thumbler index computation.
+                 */
+
+                tmp = cur.getPreviousSibling();
+                while(tmp != null) {
+                    if(tmp.getNodeType() == Node.TEXT_NODE ||
+                            tmp.getNodeType() == Node.CDATA_SECTION_NODE) {
+                        occur++;
+                    }
+                    tmp = tmp.getPreviousSibling();
+                }
+
+                if(occur == 0) {
+                    tmp = cur.getNextSibling();
+
+                    while(tmp != null && occur == 0) {
+                        if(tmp.getNodeType() == Node.TEXT_NODE ||
+                                tmp.getNodeType() == Node.CDATA_SECTION_NODE) {
+                            occur++;
+                        }
+                        tmp = tmp.getNextSibling();
+                    }
+                } else {
+                    occur++;
+                }
+
+            } else if(cur.getNodeType() == Node.PROCESSING_INSTRUCTION_NODE) {
+                sep = "/";
+                name = "processing-instruction('"+cur.getLocalName()+"')";
+                next = cur.getParentNode();
+
+                /*
+                 * Thumbler index computation.
+                 */
+
+                tmp = cur.getParentNode();
+
+                while(tmp != null) {
+                    if(tmp.getNodeType() == Node.PROCESSING_INSTRUCTION_NODE &&
+                            tmp.getLocalName().equals(cur.getLocalName())) {
+                        occur++;
+                    }
+                    tmp = tmp.getPreviousSibling();
+                }
+
+                if(occur == 0) {
+                    tmp = cur.getNextSibling();
+
+                    while(tmp != null && occur == 0) {
+                        if(tmp.getNodeType() == Node.PROCESSING_INSTRUCTION_NODE &&
+                                tmp.getLocalName().equals(cur.getLocalName())){
+                            occur++;
+                        }
+                        tmp = tmp.getNextSibling();
+                    }
+
+                    if(occur != 0) {
+                        occur = 1;
+                    }
+
+                } else {
+                    occur++;
+                }
+
+            } else if(cur.getNodeType() == Node.ATTRIBUTE_NODE) {
+                sep = "/@";
+                name = cur.getLocalName();
+
+                if(cur.getNamespaceURI() != null) {
+                    if(cur.getPrefix() != null) {
+                        name = cur.getPrefix() + ":" + name;
+                    }
+                }
+
+                next = ((Attr) cur).getOwnerElement();
+
+            } else {
+                next = cur.getParentNode();
+            }
+
+            if(occur == 0){
+                buffer = sep+name+buffer;
+            } else {
+                buffer = sep+name+"["+occur+"]"+buffer;
+            }
+
+            cur = next;
+
+        } while(cur != null);
+
+        return buffer;
     }
 
-    private static String getNodeCompletePath(Node node, String remainder) {
-        String currentPath = "";
-        if(!node.getOwnerDocument().equals(node.getParentNode())) {
-            int pos = 1;
-            boolean uniq = true;
-            //TODO: Finish
-        }
-        return node.getNodeName();
+    protected static boolean compareTwoNodes(Node m, Node n) {
+        return (m.getLocalName().equals(n.getLocalName()) &&
+                (((m.getPrefix() == null) && (n.getPrefix() == null)) ||
+                (m.getPrefix() != null) && (n.getPrefix() != null) &&
+                (n.getPrefix().equals(m.getPrefix()))));
     }
 
     public static String getNodeName(Node node) {
