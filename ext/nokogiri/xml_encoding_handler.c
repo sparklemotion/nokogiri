38fdb08532bf07a2b46c9b08b07082f25578d85d
Aaron Patterson <aaron.patterson@gmail.com>
2009-11-09 16:33:11 -0800

    can clear up aliases

--- ext/nokogiri/xml_encoding_handler.c
+++ ext/nokogiri/xml_encoding_handler.c
@@ -1,12 +1,57 @@
 #include <xml_encoding_handler.h>
 
-static VALUE get(VALUE self, VALUE key)
+/*
+ * call-seq: Nokogiri::EncodingHandler#[](name)
+ *
+ * Get the encoding handler for +name+
+ */
+static VALUE get(VALUE klass, VALUE key)
 {
   xmlCharEncodingHandlerPtr handler;
-  
+
   handler = xmlFindCharEncodingHandler(StringValuePtr(key));
-  if(handler) return Qtrue;
+  if(handler)
+    return Data_Wrap_Struct(klass, NULL, NULL, handler);
+
   return Qnil;
+}
+
+/*
+ * call-seq: Nokogiri::EncodingHandler.alias(from, to)
+ *
+ * Alias encoding handler with name +from+ to name +to+
+ */
+static VALUE alias(VALUE klass, VALUE from, VALUE to)
+{
+  xmlAddEncodingAlias(StringValuePtr(from), StringValuePtr(to));
+
+  return to;
+}
+
+/*
+ * call-seq: Nokogiri::EncodingHandler.clear_aliases!
+ *
+ * Remove all encoding aliases.
+ */
+static VALUE clear_aliases(VALUE klass)
+{
+  xmlCleanupEncodingAliases();
+
+  return klass;
+}
+
+/*
+ * call-seq: name
+ *
+ * Get the name of this EncodingHandler
+ */
+static VALUE name(VALUE self)
+{
+  xmlCharEncodingHandlerPtr handler;
+
+  Data_Get_Struct(self, xmlCharEncodingHandler, handler);
+
+  return NOKOGIRI_STR_NEW2(handler->name);
 }
 
 void init_xml_encoding_handler()
@@ -15,4 +60,7 @@ void init_xml_encoding_handler()
   VALUE klass = rb_define_class_under(nokogiri, "EncodingHandler", rb_cObject);
 
   rb_define_singleton_method(klass, "[]", get, 1);
+  rb_define_singleton_method(klass, "alias", alias, 2);
+  rb_define_singleton_method(klass, "clear_aliases!", clear_aliases, 0);
+  rb_define_method(klass, "name", name, 0);
 }
