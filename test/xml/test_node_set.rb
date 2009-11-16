760ea12779a35862d8569e944e601fa0f13f2a84
Mike Dalessio <mike@csa.net>
2009-11-16 11:42:57 -0500

    introducing failing tests for node sets containing namespace declarations.

--- test/xml/test_node_set.rb
+++ test/xml/test_node_set.rb
@@ -526,6 +526,37 @@ module Nokogiri
           inspected
       end
 
+      def test_should_not_splode_when_accessing_namespace_declarations_in_a_node_set
+        xml = Nokogiri::XML "<foo></foo>"
+        node_set = xml.xpath("//namespace::*")
+        assert_equal 1, node_set.size
+        node = node_set.first
+        node.to_s # segfaults in 1.4.0 and earlier
+
+        # if we haven't segfaulted, let's make sure we handled it correctly
+        assert_instance_of Nokogiri::XML::Namespace, node
+      end
+
+      def test_should_not_splode_when_arrayifying_node_set_containing_namespace_declarations
+        xml = Nokogiri::XML "<foo></foo>"
+        node_set = xml.xpath("//namespace::*")
+        assert_equal 1, node_set.size
+
+        node_array = node_set.to_a
+        node = node_array.first
+        node.to_s # segfaults in 1.4.0 and earlier
+
+        # if we haven't segfaulted, let's make sure we handled it correctly
+        assert_instance_of Nokogiri::XML::Namespace, node
+      end
+
+      def test_should_not_splode_when_unlinking_node_set_containing_namespace_declarations
+        xml = Nokogiri::XML "<foo></foo>"
+        node_set = xml.xpath("//namespace::*")
+        assert_equal 1, node_set.size
+
+        node_set.unlink
+      end
     end
   end
 end
