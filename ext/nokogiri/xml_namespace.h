8111e7bc40f28caeb2adbcfe098e2ff11b95c08f
Mike Dalessio <mike@csa.net>
2009-11-16 11:43:25 -0500

    C: making node set namespace decl tests pass

--- ext/nokogiri/xml_namespace.h
+++ ext/nokogiri/xml_namespace.h
@@ -8,5 +8,6 @@ void init_xml_namespace();
 extern VALUE cNokogiriXmlNamespace ;
 
 VALUE Nokogiri_wrap_xml_namespace(xmlDocPtr doc, xmlNsPtr node) ;
+VALUE Nokogiri_wrap_xml_namespace2(VALUE document, xmlNsPtr node) ;
 
 #endif
