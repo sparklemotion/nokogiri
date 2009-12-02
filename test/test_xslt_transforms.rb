84a2986cb0c0be3b80f690f7ba39ac6761e3ccf3
Aaron Patterson <aaron.patterson@gmail.com>
2009-12-01 19:45:57 -0800

    using serialize in XSLT#apply_to so that xslt output methods are honored. closes #175

--- test/test_xslt_transforms.rb
+++ test/test_xslt_transforms.rb
@@ -2,23 +2,23 @@ require "helper"
 
 class TestXsltTransforms < Nokogiri::TestCase
 
+  def setup
+    @doc = Nokogiri::XML(File.open(XML_FILE))
+  end
+
   if Nokogiri::VERSION_INFO['libxml']['loaded'] > '2.6.16'
 
     def test_class_methods
-      doc   = Nokogiri::XML(File.read(XML_FILE))
       style = Nokogiri::XSLT(File.read(XSLT_FILE))
 
-      assert result = style.apply_to(doc, ['title', '"Grandma"'])
+      assert result = style.apply_to(@doc, ['title', '"Grandma"'])
       assert_match %r{<h1>Grandma</h1>}, result
     end
 
     def test_transform
-      assert doc = Nokogiri::XML.parse(File.read(XML_FILE))
-      assert doc.xml?
-
       assert style = Nokogiri::XSLT.parse(File.read(XSLT_FILE))
 
-      assert result = style.apply_to(doc, ['title', '"Booyah"'])
+      assert result = style.apply_to(@doc, ['title', '"Booyah"'])
       assert_match %r{<h1>Booyah</h1>}, result
       assert_match %r{<th.*Employee ID</th>}, result
       assert_match %r{<th.*Name</th>}, result
@@ -31,41 +31,68 @@ class TestXsltTransforms < Nokogiri::TestCase
       assert_no_match %r{Dallas|Texas}, result
       assert_no_match %r{Female}, result
 
-      assert result = style.apply_to(doc, ['title', '"Grandma"'])
+      assert result = style.apply_to(@doc, ['title', '"Grandma"'])
       assert_match %r{<h1>Grandma</h1>}, result
 
-      assert result = style.apply_to(doc)
+      assert result = style.apply_to(@doc)
       assert_match %r{<h1></h1>}, result
     end
 
-    def test_transform2
-      assert doc = Nokogiri::XML.parse(File.read(XML_FILE))
-      assert doc.xml?
+    def test_transform_with_output_style
+      xslt = Nokogiri::XSLT(<<-eoxslt)
+<?xml version="1.0" encoding="ISO-8859-1"?>
 
-      assert style = Nokogiri::XSLT.parse(File.read(XSLT_FILE))
-      assert result_doc = style.transform(doc)
+<xsl:stylesheet version="1.0"
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="text" version="1.0"
+encoding="iso-8859-1" indent="yes"/>
+
+<xsl:param name="title"/>
+
+<xsl:template match="/">
+  <html>
+  <body>
+    <xsl:for-each select="staff/employee">
+    <tr>
+      <td><xsl:value-of select="employeeId"/></td>
+      <td><xsl:value-of select="name"/></td>
+      <td><xsl:value-of select="position"/></td>
+      <td><xsl:value-of select="salary"/></td>
+    </tr>
+    </xsl:for-each>
+    </table>
+  </body>
+  </html>
+</xsl:template>
+
+</xsl:stylesheet>
+      eoxslt
+      assert_no_match(/<td>/, xslt.apply_to(@doc, ['title', 'foo']))
+    end
+
+    def test_transform2
+      assert style = Nokogiri::XSLT(File.open(XSLT_FILE))
+      assert result_doc = style.transform(@doc)
       assert result_doc.html?
       assert_equal "", result_doc.at_css("h1").content
 
-      assert style = Nokogiri::XSLT.parse(File.read(XSLT_FILE))
-      assert result_doc = style.transform(doc, ['title', '"Booyah"'])
+      assert style = Nokogiri::XSLT(File.read(XSLT_FILE))
+      assert result_doc = style.transform(@doc, ['title', '"Booyah"'])
       assert result_doc.html?
       assert_equal "Booyah", result_doc.at_css("h1").content
 
-      assert result_string = style.apply_to(doc, ['title', '"Booyah"'])
+      assert result_string = style.apply_to(@doc, ['title', '"Booyah"'])
       assert_equal result_string, style.serialize(result_doc)
     end
 
     def test_transform_with_quote_params
-      doc = Nokogiri::XML.parse(File.read(XML_FILE))
-
-      assert style = Nokogiri::XSLT.parse(File.read(XSLT_FILE))
-      assert result_doc = style.transform(doc, Nokogiri::XSLT.quote_params(['title', 'Booyah']))
+      assert style = Nokogiri::XSLT(File.open(XSLT_FILE))
+      assert result_doc = style.transform(@doc, Nokogiri::XSLT.quote_params(['title', 'Booyah']))
       assert result_doc.html?
       assert_equal "Booyah", result_doc.at_css("h1").content
 
       assert style = Nokogiri::XSLT.parse(File.read(XSLT_FILE))
-      assert result_doc = style.transform(doc, Nokogiri::XSLT.quote_params({'title' => 'Booyah'}))
+      assert result_doc = style.transform(@doc, Nokogiri::XSLT.quote_params({'title' => 'Booyah'}))
       assert result_doc.html?
       assert_equal "Booyah", result_doc.at_css("h1").content
     end
@@ -80,7 +107,7 @@ class TestXsltTransforms < Nokogiri::TestCase
       }
       hh=h.dup
       result_hash = Nokogiri::XSLT.quote_params(h)
-      assert_equal hh, h # non-destructive 
+      assert_equal hh, h # non-destructive
 
       a=h.to_a.flatten
       result_array = Nokogiri::XSLT.quote_params(a)
@@ -92,17 +119,17 @@ class TestXsltTransforms < Nokogiri::TestCase
     def test_exslt
       assert doc = Nokogiri::XML.parse(File.read(EXML_FILE))
       assert doc.xml?
-      
+
       assert style = Nokogiri::XSLT.parse(File.read(EXSLT_FILE))
-      params = { 
+      params = {
         :p1 => 'xxx',
         :p2 => "x'x'x",
         :p3 => 'x"x"x',
         :p4 => '"xxx"'
       }
-      result_doc = Nokogiri::XML.parse(style.apply_to(doc, 
+      result_doc = Nokogiri::XML.parse(style.apply_to(doc,
           Nokogiri::XSLT.quote_params(params)))
-      
+
       assert_equal 'func-result', result_doc.at('/root/function').content
       assert_equal 3, result_doc.at('/root/max').content.to_i
       assert_match(
@@ -113,11 +140,11 @@ class TestXsltTransforms < Nokogiri::TestCase
         assert_equal p.content, params[p.name.intern]
       end
       check_params result_doc, params
-      result_doc = Nokogiri::XML.parse(style.apply_to(doc, 
+      result_doc = Nokogiri::XML.parse(style.apply_to(doc,
           Nokogiri::XSLT.quote_params(params.to_a.flatten)))
       check_params result_doc, params
     end
-    
+
     def test_xslt_parse_error
       xslt_str = <<-EOX
 <xsl:stylesheet version="1.0"
