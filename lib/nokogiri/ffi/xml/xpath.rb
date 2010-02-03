8699878238a68a35dfa171bb1ce60790a03583ba
Mike Dalessio <mike@csa.net>
2010-02-03 01:18:55 -0500

    removing unused FFI code

--- lib/nokogiri/ffi/xml/xpath.rb
+++ lib/nokogiri/ffi/xml/xpath.rb
@@ -4,13 +4,6 @@ module Nokogiri
       
       attr_accessor :cstruct # :nodoc:
 
-      def node_set # :nodoc:
-        ptr = cstruct[:nodesetval] if cstruct[:nodesetval]
-        ptr = LibXML.xmlXPathNodeSetCreate(nil) if ptr.null?
-
-        NodeSet.wrap(ptr, @document)
-      end
-
     end
   end
 end
