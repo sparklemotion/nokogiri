f4c7f36db703b10aacb893163bf82bed9d5e3f65
Yoko Harada <yokolet@gmail.com>
2010-04-29 10:34:15 -0400

    Return of commented out test cases of html/test_document.rb. Also, I added a missing test case, test_round_trip, to the same file. Since the test is among commented out tests, probably, git merge failed to add the test case.

--- test/html/test_document.rb
+++ test/html/test_document.rb
@@ -117,14 +117,14 @@ module Nokogiri
         }
       end
 
-#      def test_meta_encoding
-#        assert_equal 'UTF-8', @html.meta_encoding
-#      end
-#
-#      def test_meta_encoding=
-#        @html.meta_encoding = 'EUC-JP'
-#        assert_equal 'EUC-JP', @html.meta_encoding
-#      end
+      def test_meta_encoding
+        assert_equal 'UTF-8', @html.meta_encoding
+      end
+
+      def test_meta_encoding=
+        @html.meta_encoding = 'EUC-JP'
+        assert_equal 'EUC-JP', @html.meta_encoding
+      end
 
       def test_meta_encoding_without_head
         html = Nokogiri::HTML('<html><body>foo</body></html>')
@@ -182,35 +182,35 @@ module Nokogiri
         assert_no_match(/^<\?xml/, html.to_html)
       end
 
-#      def test_document_has_error
-#        html = Nokogiri::HTML(<<-eohtml)
-#        <html>
-#          <body>
-#            <div awesome="asdf>
-#              <p>inside div tag</p>
-#            </div>
-#            <p>outside div tag</p>
-#          </body>
-#        </html>
-#        eohtml
-#        assert html.errors.length > 0
-#      end
-#
-#      def test_relative_css
-#        html = Nokogiri::HTML(<<-eohtml)
-#        <html>
-#          <body>
-#            <div>
-#              <p>inside div tag</p>
-#            </div>
-#            <p>outside div tag</p>
-#          </body>
-#        </html>
-#        eohtml
-#        set = html.search('div').search('p')
-#        assert_equal(1, set.length)
-#        assert_equal('inside div tag', set.first.inner_text)
-#      end
+      def test_document_has_error
+        html = Nokogiri::HTML(<<-eohtml)
+        <html>
+          <body>
+            <div awesome="asdf>
+              <p>inside div tag</p>
+            </div>
+            <p>outside div tag</p>
+          </body>
+        </html>
+        eohtml
+        assert html.errors.length > 0
+      end
+
+      def test_relative_css
+        html = Nokogiri::HTML(<<-eohtml)
+        <html>
+          <body>
+            <div>
+              <p>inside div tag</p>
+            </div>
+            <p>outside div tag</p>
+          </body>
+        </html>
+        eohtml
+        set = html.search('div').search('p')
+        assert_equal(1, set.length)
+        assert_equal('inside div tag', set.first.inner_text)
+      end
 
       def test_multi_css
         html = Nokogiri::HTML(<<-eohtml)
@@ -228,59 +228,59 @@ module Nokogiri
         assert_equal ['a tag', 'p tag'].sort, set.map { |x| x.content }.sort
       end
 
-#      def test_inner_text
-#        html = Nokogiri::HTML(<<-eohtml)
-#        <html>
-#          <body>
-#            <div>
-#              <p>
-#                Hello world!
-#              </p>
-#            </div>
-#          </body>
-#        </html>
-#        eohtml
-#        node = html.xpath('//div').first
-#        assert_equal('Hello world!', node.inner_text.strip)
-#      end
-#
-#      def test_find_by_xpath
-#        found = @html.xpath('//div/a')
-#        assert_equal 3, found.length
-#      end
-#
-#      def test_find_by_css
-#        found = @html.css('div > a')
-#        assert_equal 3, found.length
-#      end
-#
-#      def test_find_by_css_with_square_brackets
-#        found = @html.css("div[@id='header'] > h1")
-#        found = @html.css("div[@id='header'] h1") # this blows up on commit 6fa0f6d329d9dbf1cc21c0ac72f7e627bb4c05fc
-#        assert_equal 1, found.length
-#      end
-#
-#      def test_find_with_function
-#        found = @html.css("div:awesome() h1", Class.new {
-#          def awesome divs
-#            [divs.first]
-#          end
-#        }.new)
-#      end
-#
-#      def test_dup_shallow
-#        found = @html.search('//div/a').first
-#        dup = found.dup(0)
-#        assert dup
-#        assert_equal '', dup.content
-#      end
-#
-#      def test_search_can_handle_xpath_and_css
-#        found = @html.search('//div/a', 'div > p')
-#        length = @html.xpath('//div/a').length +
-#          @html.css('div > p').length
-#        assert_equal length, found.length
-#      end
+      def test_inner_text
+        html = Nokogiri::HTML(<<-eohtml)
+        <html>
+          <body>
+            <div>
+              <p>
+                Hello world!
+              </p>
+            </div>
+          </body>
+        </html>
+        eohtml
+        node = html.xpath('//div').first
+        assert_equal('Hello world!', node.inner_text.strip)
+      end
+
+      def test_find_by_xpath
+        found = @html.xpath('//div/a')
+        assert_equal 3, found.length
+      end
+
+      def test_find_by_css
+        found = @html.css('div > a')
+        assert_equal 3, found.length
+      end
+
+      def test_find_by_css_with_square_brackets
+        found = @html.css("div[@id='header'] > h1")
+        found = @html.css("div[@id='header'] h1") # this blows up on commit 6fa0f6d329d9dbf1cc21c0ac72f7e627bb4c05fc
+        assert_equal 1, found.length
+      end
+
+      def test_find_with_function
+        found = @html.css("div:awesome() h1", Class.new {
+          def awesome divs
+            [divs.first]
+          end
+        }.new)
+      end
+
+      def test_dup_shallow
+        found = @html.search('//div/a').first
+        dup = found.dup(0)
+        assert dup
+        assert_equal '', dup.content
+      end
+
+      def test_search_can_handle_xpath_and_css
+        found = @html.search('//div/a', 'div > p')
+        length = @html.xpath('//div/a').length +
+          @html.css('div > p').length
+        assert_equal length, found.length
+      end
 
       def test_dup_document
         assert dup = @html.dup
@@ -296,79 +296,84 @@ module Nokogiri
         assert_not_equal dup, @html
       end
 
-#      def test_dup
-#        found = @html.search('//div/a').first
-#        dup = found.dup
-#        assert dup
-#        assert_equal found.content, dup.content
-#        assert_equal found.document, dup.document
-#      end
-#
-#      def test_inner_html
-#        html = Nokogiri::HTML(<<-eohtml)
-#        <html>
-#          <body>
-#            <div>
-#              <p>
-#                Hello world!
-#              </p>
-#            </div>
-#          </body>
-#        </html>
-#        eohtml
-#        node = html.xpath('//div').first
-#        assert_equal('<p>Helloworld!</p>', node.inner_html.gsub(/\s/, ''))
-#      end
-#
-#      def test_fragment_contains_text_node
-#        fragment = Nokogiri::HTML.fragment('fooo')
-#        assert_equal 1, fragment.children.length
-#        assert_equal 'fooo', fragment.inner_text
-#      end
-#
-#      def test_fragment_includes_two_tags
-#        assert_equal 2, Nokogiri::HTML.fragment("<br/><hr/>").children.length
-#      end
-#
-#      def test_relative_css_finder
-#        doc = Nokogiri::HTML(<<-eohtml)
-#          <html>
-#            <body>
-#              <div class="red">
-#                <p>
-#                  inside red
-#                </p>
-#              </div>
-#              <div class="green">
-#                <p>
-#                  inside green
-#                </p>
-#              </div>
-#            </body>
-#          </html>
-#        eohtml
-#        red_divs = doc.css('div.red')
-#        assert_equal 1, red_divs.length
-#        p_tags = red_divs.first.css('p')
-#        assert_equal 1, p_tags.length
-#        assert_equal 'inside red', p_tags.first.text.strip
-#      end
-#
-#      def test_find_classes
-#        doc = Nokogiri::HTML(<<-eohtml)
-#          <html>
-#            <body>
-#              <p class="red">RED</p>
-#              <p class="awesome red">RED</p>
-#              <p class="notred">GREEN</p>
-#              <p class="green notred">GREEN</p>
-#            </body>
-#          </html>
-#        eohtml
-#        list = doc.css('.red')
-#        assert_equal 2, list.length
-#        assert_equal %w{ RED RED }, list.map { |x| x.text }
-#      end
+      def test_dup
+        found = @html.search('//div/a').first
+        dup = found.dup
+        assert dup
+        assert_equal found.content, dup.content
+        assert_equal found.document, dup.document
+      end
+
+      def test_inner_html
+        html = Nokogiri::HTML(<<-eohtml)
+        <html>
+          <body>
+            <div>
+              <p>
+                Hello world!
+              </p>
+            </div>
+          </body>
+        </html>
+        eohtml
+        node = html.xpath('//div').first
+        assert_equal('<p>Helloworld!</p>', node.inner_html.gsub(/\s/, ''))
+      end
+
+      def test_round_trip
+        doc = Nokogiri::HTML(@html.inner_html)
+        assert_equal @html.root.to_html, doc.root.to_html
+      end
+
+      def test_fragment_contains_text_node
+        fragment = Nokogiri::HTML.fragment('fooo')
+        assert_equal 1, fragment.children.length
+        assert_equal 'fooo', fragment.inner_text
+      end
+
+      def test_fragment_includes_two_tags
+        assert_equal 2, Nokogiri::HTML.fragment("<br/><hr/>").children.length
+      end
+
+      def test_relative_css_finder
+        doc = Nokogiri::HTML(<<-eohtml)
+          <html>
+            <body>
+              <div class="red">
+                <p>
+                  inside red
+                </p>
+              </div>
+              <div class="green">
+                <p>
+                  inside green
+                </p>
+              </div>
+            </body>
+          </html>
+        eohtml
+        red_divs = doc.css('div.red')
+        assert_equal 1, red_divs.length
+        p_tags = red_divs.first.css('p')
+        assert_equal 1, p_tags.length
+        assert_equal 'inside red', p_tags.first.text.strip
+      end
+
+      def test_find_classes
+        doc = Nokogiri::HTML(<<-eohtml)
+          <html>
+            <body>
+              <p class="red">RED</p>
+              <p class="awesome red">RED</p>
+              <p class="notred">GREEN</p>
+              <p class="green notred">GREEN</p>
+            </body>
+          </html>
+        eohtml
+        list = doc.css('.red')
+        assert_equal 2, list.length
+        assert_equal %w{ RED RED }, list.map { |x| x.text }
+      end
 
       def test_parse_can_take_io
         html = nil
