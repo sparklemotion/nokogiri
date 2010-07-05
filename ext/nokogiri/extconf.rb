ebbd415a09db641be3632d99848ddb86d3e99a1c
Romulo A. Ceccon <romuloceccon@gmail.com>
2010-07-05 22:41:49 +0800

    made extconf.rb and Rakefile Windows friendly; fixed Manifest.txt entries (cherry picked from commit 6d47d388dc5e0a752b74c5c3172b220d2873f83f)

--- ext/nokogiri/extconf.rb
+++ ext/nokogiri/extconf.rb
@@ -17,7 +17,7 @@ end
 $CFLAGS << " #{ENV["CFLAGS"]}"
 $LIBS << " #{ENV["LIBS"]}"
 
-if Config::CONFIG['target_os'] == 'mingw32'
+if Config::CONFIG['target_os'] == 'mingw32' || Config::CONFIG['target_os'] =~ /mswin32/
   $CFLAGS << " -DXP_WIN -DXP_WIN32 -DUSE_INCLUDED_VASPRINTF"
 elsif Config::CONFIG['target_os'] =~ /solaris/
   $CFLAGS << " -DUSE_INCLUDED_VASPRINTF"
@@ -29,43 +29,60 @@ if RbConfig::MAKEFILE_CONFIG['CC'] =~ /mingw/
   $CFLAGS << " -DIN_LIBXML"
 end
 
-$CFLAGS << " -O3 -Wall -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-noreturn -Winline"
+if Config::CONFIG['target_os'] =~ /mswin32/
+  lib_prefix = 'lib'
+  
+  # There's no default include/lib dir on Windows. Let's just add the Ruby ones
+  # and resort on the search path specified by INCLUDE and LIB environment
+  # variables
+  HEADER_DIRS = [INCLUDEDIR]
+  LIB_DIRS = [LIBDIR]
+  XML2_HEADER_DIRS = [File.join(INCLUDEDIR, "libxml2"), INCLUDEDIR]
+  
+else
+  lib_prefix = ''
+  
+  $CFLAGS << " -O3 -Wall -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-noreturn -Winline"
 
-HEADER_DIRS = [
-  # First search /opt/local for macports
-  '/opt/local/include',
+  HEADER_DIRS = [
+    # First search /opt/local for macports
+    '/opt/local/include',
 
-  # Then search /usr/local for people that installed from source
-  '/usr/local/include',
+    # Then search /usr/local for people that installed from source
+    '/usr/local/include',
 
-  # Check the ruby install locations
-  INCLUDEDIR,
+    # Check the ruby install locations
+    INCLUDEDIR,
 
-  # Finally fall back to /usr
-  '/usr/include',
-  '/usr/include/libxml2',
-]
+    # Finally fall back to /usr
+    '/usr/include',
+    '/usr/include/libxml2',
+  ]
 
-LIB_DIRS = [
-  # First search /opt/local for macports
-  '/opt/local/lib',
+  LIB_DIRS = [
+    # First search /opt/local for macports
+    '/opt/local/lib',
 
-  # Then search /usr/local for people that installed from source
-  '/usr/local/lib',
+    # Then search /usr/local for people that installed from source
+    '/usr/local/lib',
 
-  # Check the ruby install locations
-  LIBDIR,
+    # Check the ruby install locations
+    LIBDIR,
 
-  # Finally fall back to /usr
-  '/usr/lib',
-]
+    # Finally fall back to /usr
+    '/usr/lib',
+  ]
+
+  XML2_HEADER_DIRS = [
+    '/opt/local/include/libxml2',
+    '/usr/local/include/libxml2',
+    File.join(INCLUDEDIR, "libxml2")
+  ] + HEADER_DIRS
+end
 
 dir_config('zlib', HEADER_DIRS, LIB_DIRS)
 dir_config('iconv', HEADER_DIRS, LIB_DIRS)
-dir_config('xml2', [
-           '/opt/local/include/libxml2',
-           '/usr/local/include/libxml2',
-           File.join(INCLUDEDIR, "libxml2")] + HEADER_DIRS, LIB_DIRS)
+dir_config('xml2', XML2_HEADER_DIRS, LIB_DIRS)
 dir_config('xslt', HEADER_DIRS, LIB_DIRS)
 
 def asplode(lib)
@@ -80,9 +97,9 @@ pkg_config('libxslt') if RUBY_PLATFORM =~ /mingw/
 asplode "libxml2"  unless find_header('libxml/parser.h')
 asplode "libxslt"  unless find_header('libxslt/xslt.h')
 asplode "libexslt" unless find_header('libexslt/exslt.h')
-asplode "libxml2"  unless find_library('xml2', 'xmlParseDoc')
-asplode "libxslt"  unless find_library('xslt', 'xsltParseStylesheetDoc')
-asplode "libexslt" unless find_library('exslt', 'exsltFuncRegister')
+asplode "libxml2"  unless find_library("#{lib_prefix}xml2", 'xmlParseDoc')
+asplode "libxslt"  unless find_library("#{lib_prefix}xslt", 'xsltParseStylesheetDoc')
+asplode "libexslt" unless find_library("#{lib_prefix}exslt", 'exsltFuncRegister')
 
 have_func 'xmlFirstElementChild'
 have_func('xmlRelaxNGSetParserStructuredErrors')
