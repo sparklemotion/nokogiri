cf2123ca1c3e69d1029ba9c70d1d1281cec20a0a
Aaron Patterson <aaron.patterson@gmail.com>
2011-05-10 16:22:24 -0700

    adding a test to tickle a builder bug in JRuby

--- test/xml/test_builder.rb
+++ test/xml/test_builder.rb
@@ -3,6 +3,14 @@ require "helper"
 module Nokogiri
   module XML
     class TestBuilder < Nokogiri::TestCase
+      def test_attribute_sensitivity
+        xml = Nokogiri::XML::Builder.new { |xml|
+          xml.tag "hello", "abcDef" => "world"
+        }.to_xml
+        doc = Nokogiri.XML xml
+        assert_equal 'world', doc.root['abcDef']
+      end
+
       def test_builder_escape
         xml = Nokogiri::XML::Builder.new { |xml|
           xml.condition "value < 1", :attr => "value < 1"
