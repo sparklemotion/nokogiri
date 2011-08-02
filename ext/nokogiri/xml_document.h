c1cfce431f14f341cf2055b3f6e7f2e453c705e4
Timothy Elliott <tle@holymonkey.com>
2011-08-02 09:23:11 -0700

    The member doc of nokogiriTuple is a VALUE, not a libxml doc pointer
    Yes, you had me fooled for a while, but I caught on. Had a few
    "how the heck does this work?" moments though.

--- ext/nokogiri/xml_document.h
+++ ext/nokogiri/xml_document.h
@@ -4,7 +4,7 @@
 #include <nokogiri.h>
 
 struct _nokogiriTuple {
-  xmlDocPtr     doc;
+  VALUE         doc;
   st_table     *unlinkedNodes;
   VALUE         node_cache;
 };
@@ -15,9 +15,9 @@ void init_xml_document();
 VALUE Nokogiri_wrap_xml_document(VALUE klass, xmlDocPtr doc);
 
 #define DOC_RUBY_OBJECT_TEST(x) ((nokogiriTuplePtr)(x->_private))
-#define DOC_RUBY_OBJECT(x) ((VALUE)((nokogiriTuplePtr)(x->_private))->doc)
+#define DOC_RUBY_OBJECT(x) (((nokogiriTuplePtr)(x->_private))->doc)
 #define DOC_UNLINKED_NODE_HASH(x) (((nokogiriTuplePtr)(x->_private))->unlinkedNodes)
-#define DOC_NODE_CACHE(x) ((VALUE)((nokogiriTuplePtr)(x->_private))->node_cache)
+#define DOC_NODE_CACHE(x) (((nokogiriTuplePtr)(x->_private))->node_cache)
 
 extern VALUE cNokogiriXmlDocument ;
 #endif
