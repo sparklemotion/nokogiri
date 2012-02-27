b846d01ff607fb92d43a795736128b30b8976fbd
Mike Dalessio <mike@csa.net>
2012-02-27 09:21:46 -0500

    Updating the epic comment re: freeing xmlNodeSet, and moving internal methods to the start of the file.

--- ext/nokogiri/xml_node_set.c
+++ ext/nokogiri/xml_node_set.c
@@ -3,6 +3,69 @@
 
 static ID decorate ;
 
+static int dealloc_namespace(xmlNsPtr ns)
+{
+  if (ns->href)
+    xmlFree((xmlChar *)ns->href);
+  if (ns->prefix)
+    xmlFree((xmlChar *)ns->prefix);
+  xmlFree(ns);
+  return ST_CONTINUE;
+}
+
+static void deallocate(nokogiriNodeSetTuple *tuple)
+{
+  /*
+   *  xmlXPathFreeNodeSet() contains an implicit assumption that it is being
+   *  called before any of its pointed-to nodes have been free()d. this
+   *  assumption lies in the operation where it dereferences nodeTab pointers
+   *  while searching for namespace nodes to free.
+   *
+   *  however, since Ruby's GC mechanism cannot guarantee the strict order in
+   *  which ruby objects will be GC'd, nodes may be garbage collected before a
+   *  nodeset containing pointers to those nodes. (this is true regardless of
+   *  how we declare dependencies between objects with rb_gc_mark().)
+   *
+   *  as a result, xmlXPathFreeNodeSet() will perform unsafe memory operations,
+   *  and calling it would be evil.
+   *
+   *  so here, we *manually* free the set of namespace nodes that was
+   *  constructed at initialization time (see Nokogiri_wrap_xml_node_set()), as
+   *  well as the NodeSet, without using the official xmlXPathFreeNodeSet().
+   *
+   *  there's probably a lesson in here somewhere about intermingling, within a
+   *  single array, structs with different memory-ownership semantics. or more
+   *  generally, a lesson about building an API in C/C++ that does not contain
+   *  assumptions about the strict order in which memory will be released. hey,
+   *  that sounds like a great idea for a blog post! get to it!
+   *
+   *  "In Valgrind We Trust." seriously.
+   */
+  xmlNodeSetPtr node_set;
+
+  node_set = tuple->node_set;
+
+  if (!node_set)
+    return;
+
+  NOKOGIRI_DEBUG_START(node_set) ;
+  st_foreach(tuple->namespaces, dealloc_namespace, 0);
+
+  if (node_set->nodeTab != NULL)
+    xmlFree(node_set->nodeTab);
+
+  xmlFree(node_set);
+  st_free_table(tuple->namespaces);
+  free(tuple);
+  NOKOGIRI_DEBUG_END(node_set) ;
+}
+
+static VALUE allocate(VALUE klass)
+{
+  return Nokogiri_wrap_xml_node_set(xmlXPathNodeSetCreate(NULL), Qnil);
+}
+
+
 /*
  * call-seq:
  *  dup
@@ -345,72 +408,6 @@ static VALUE unlink_nodeset(VALUE self)
   return self ;
 }
 
-static int dealloc_namespace(xmlNsPtr ns)
-{
-  if (ns->href)
-    xmlFree((xmlChar *)ns->href);
-  if (ns->prefix)
-    xmlFree((xmlChar *)ns->prefix);
-  xmlFree(ns);
-  return ST_CONTINUE;
-}
-
-static void deallocate(nokogiriNodeSetTuple *tuple)
-{
-  /*
-   *  xmlXPathFreeNodeSet() contains an implicit assumption that it is being
-   *  called before any of its pointed-to nodes have been free()d. this
-   *  assumption lies in the operation where it dereferences nodeTab pointers
-   *  while searching for namespace nodes to free.
-   *
-   *  however, since Ruby's GC mechanism cannot guarantee the strict order in
-   *  which ruby objects will be GC'd, nodes may be garbage collected before a
-   *  nodeset containing pointers to those nodes. (this is true regardless of
-   *  how we declare dependencies between objects with rb_gc_mark().)
-   *
-   *  as a result, xmlXPathFreeNodeSet() will perform unsafe memory operations,
-   *  and calling it would be evil.
-   *
-   *  on the bright side, though, Nokogiri's API currently does not cause
-   *  namespace nodes to be included in node sets, ever.
-   *
-   *  armed with that fact, we examined xmlXPathFreeNodeSet() and related libxml
-   *  code and determined that, within the Nokogiri abstraction, we will not
-   *  leak memory if we simply free the node set's memory directly. that's only
-   *  quasi-evil!
-   *
-   *  there's probably a lesson in here somewhere about intermingling, within a
-   *  single array, structs with different memory-ownership semantics. or more
-   *  generally, a lesson about building an API in C/C++ that does not contain
-   *  assumptions about the strict order in which memory will be released. hey,
-   *  that sounds like a great idea for a blog post! get to it!
-   *
-   *  "In Valgrind We Trust." seriously.
-   */
-  xmlNodeSetPtr node_set;
-
-  node_set = tuple->node_set;
-
-  if (!node_set)
-    return;
-
-  NOKOGIRI_DEBUG_START(node_set) ;
-  st_foreach(tuple->namespaces, dealloc_namespace, 0);
-
-  if (node_set->nodeTab != NULL)
-    xmlFree(node_set->nodeTab);
-
-  xmlFree(node_set);
-  st_free_table(tuple->namespaces);
-  free(tuple);
-  NOKOGIRI_DEBUG_END(node_set) ;
-}
-
-static VALUE allocate(VALUE klass)
-{
-  return Nokogiri_wrap_xml_node_set(xmlXPathNodeSetCreate(NULL), Qnil);
-}
-
 VALUE Nokogiri_wrap_xml_node_set(xmlNodeSetPtr node_set, VALUE document)
 {
   VALUE new_set ;
@@ -447,9 +444,9 @@ VALUE Nokogiri_wrap_xml_node_set(xmlNodeSetPtr node_set, VALUE document)
 VALUE cNokogiriXmlNodeSet ;
 void init_xml_node_set(void)
 {
-  VALUE nokogiri  = rb_define_module("Nokogiri");
-  VALUE xml       = rb_define_module_under(nokogiri, "XML");
-  VALUE klass     = rb_define_class_under(xml, "NodeSet", rb_cObject);
+  VALUE nokogiri = rb_define_module("Nokogiri");
+  VALUE xml      = rb_define_module_under(nokogiri, "XML");
+  VALUE klass    = rb_define_class_under(xml, "NodeSet", rb_cObject);
   cNokogiriXmlNodeSet = klass;
 
   rb_define_alloc_func(klass, allocate);
@@ -466,5 +463,5 @@ void init_xml_node_set(void)
   rb_define_method(klass, "&", intersection, 1);
   rb_define_method(klass, "include?", include_eh, 1);
 
-  decorate      = rb_intern("decorate");
+  decorate = rb_intern("decorate");
 }
