sparklemotion/nokogiri

commit c5e437252ba077ef04cfd1aa64d5c486a1daf425

Fix for issue 550 (prevent siblings from being added to root)

lib/nokogiri/xml/node.rb
@@ -291,6 +291,8 @@ module Nokogiri
       #
       # Also see related method +before+.
       def add_previous_sibling node_or_tags
+        raise ArgumentError.new("A document may not have multiple root nodes.") if parent.is_a?(XML::Document)
+
         node_or_tags = coerce(node_or_tags)
         if node_or_tags.is_a?(XML::NodeSet)
           if text?
@@ -315,6 +317,8 @@ module Nokogiri
       #
       # Also see related method +after+.
       def add_next_sibling node_or_tags
+        raise ArgumentError.new("A document may not have multiple root nodes.") if parent.is_a?(XML::Document)
+        
         node_or_tags = coerce(node_or_tags)
         if node_or_tags.is_a?(XML::NodeSet)
           if text?

test/xml/test_unparented_node.rb
@@ -223,6 +223,30 @@ module Nokogiri
         right_space.add_next_sibling(left_space)
         assert_equal left_space, right_space
       end
+      
+      def test_add_next_sibling_to_root_raises_exception
+        xml = Nokogiri::XML(<<-eoxml)
+        <root />
+        eoxml
+        
+        node = Nokogiri::XML::Node.new 'child', xml
+
+        assert_raise(ArgumentError) do
+          xml.root.add_next_sibling(node)
+        end
+      end
+
+      def test_add_previous_sibling_to_root_raises_exception
+        xml = Nokogiri::XML(<<-eoxml)
+        <root />
+        eoxml
+        
+        node = Nokogiri::XML::Node.new 'child', xml
+
+        assert_raise(ArgumentError) do
+          xml.root.add_previous_sibling(node)
+        end
+      end
 
       def test_find_by_css_with_tilde_eql
         xml = Nokogiri::XML.parse(<<-eoxml)

commit 0ea3e76f8cc8857a1a146810826d936fdd18f601

Allowing insertion of processing instructions via #add_previous_sibling. Closes #630.
See related commit c5e437252ba077ef04cfd1aa64d5c486a1daf425 which broke this functionality.

lib/nokogiri/xml/node.rb
@@ -297,7 +297,7 @@ module Nokogiri
       #
       # Also see related method +before+.
       def add_previous_sibling node_or_tags
-        raise ArgumentError.new("A document may not have multiple root nodes.") if parent.is_a?(XML::Document)
+        raise ArgumentError.new("A document may not have multiple root nodes.") if parent.is_a?(XML::Document) && !node_or_tags.is_a?(XML::ProcessingInstruction)
 
         node_or_tags = coerce(node_or_tags)
         if node_or_tags.is_a?(XML::NodeSet)

test/xml/test_unparented_node.rb
@@ -248,6 +248,14 @@ module Nokogiri
         end
       end
 
+      def test_add_pi_as_previous_sibling_to_root_is_ok
+        doc = Nokogiri::XML "<root>foo</root>"
+        pi = Nokogiri::XML::ProcessingInstruction.new(doc, "xml-stylesheet", %q{type="text/xsl" href="foo.xsl"})
+        doc.root.add_previous_sibling pi
+        expected_doc = %Q{<?xml version="1.0"?>\n<?xml-stylesheet type="text/xsl" href="foo.xsl"?>\n<root>foo</root>\n}
+        assert_equal expected_doc, doc.to_xml
+      end
+
       def test_find_by_css_with_tilde_eql
         xml = Nokogiri::XML.parse(<<-eoxml)
         <root>