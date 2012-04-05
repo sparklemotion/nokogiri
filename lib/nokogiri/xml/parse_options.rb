61fb379e2decfeebaab6ce0aa41795d43bc685c4
Luis Parravicini <lparravi@gmail.com>
2012-04-05 12:43:50 -0300

    fixed typo: substitition

--- lib/nokogiri/xml/parse_options.rb
+++ lib/nokogiri/xml/parse_options.rb
@@ -25,7 +25,7 @@ module Nokogiri
       NOBLANKS    = 1 << 8
       # use the SAX1 interface internally
       SAX1        = 1 << 9
-      # Implement XInclude substitition
+      # Implement XInclude substitution
       XINCLUDE    = 1 << 10
       # Forbid network access
       NONET       = 1 << 11
