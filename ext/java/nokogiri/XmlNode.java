4dcfd73b1db96f3b40b153fa6ca6cd49b4461753
Yoko Harada <yokolet@gmail.com>
2012-06-22 23:12:27 +0900

    Fix for issue#493

--- ext/java/nokogiri/XmlNode.java
+++ ext/java/nokogiri/XmlNode.java
@@ -463,7 +463,7 @@ public class XmlNode extends RubyObject {
                     } else if ("xmlns".equals(prefix) || nodeName.equals("xmlns")) {
                         nsUri = "http://www.w3.org/2000/xmlns/";
                     } else {
-                        nsUri = attr.lookupNamespaceURI(nodeName);
+                        nsUri = attr.getNamespaceURI();
                     }
                     e.getOwnerDocument().renameNode(attr, nsUri, nodeName);
                 }
