bb59f1b0047b3f4b717833e7c43bb3acf74ad167
Bradley Schaefer <bradley.schaefer@gmail.com>
2012-09-01 12:47:11 -0500

    parse_io respects @encoding when no encoding is passed to it

--- lib/nokogiri/xml/sax/parser.rb
+++ lib/nokogiri/xml/sax/parser.rb
@@ -68,8 +68,7 @@ module Nokogiri
 
         # Create a new Parser with +doc+ and +encoding+
         def initialize doc = Nokogiri::XML::SAX::Document.new, encoding = 'UTF-8'
-          check_encoding(encoding)
-          @encoding = encoding
+          @encoding = check_encoding(encoding)
           @document = doc
           @warned   = false
         end
@@ -87,9 +86,9 @@ module Nokogiri
 
         ###
         # Parse given +io+
-        def parse_io io, encoding = 'ASCII'
-          check_encoding(encoding)
-          @encoding = encoding
+        def parse_io io, encoding = nil
+          encoding ||= @encoding
+          @encoding = check_encoding(encoding)
           ctx = ParserContext.io(io, ENCODINGS[encoding])
           yield ctx if block_given?
           ctx.parse_with self
@@ -115,6 +114,7 @@ module Nokogiri
         private
         def check_encoding(encoding)
           raise ArgumentError.new("'#{encoding}' is not a valid encoding") unless ENCODINGS[encoding]
+          encoding
         end
       end
     end
