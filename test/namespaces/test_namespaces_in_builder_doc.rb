d02691ac627c99990f5345cde44064c8230cd6e6
Michael Klein <mbklein@gmail.com>
2013-02-14 09:34:45 -0600

    Disambiguate and shore up namespace tests

--- test/namespaces/test_namespaces_in_builder_doc.rb
+++ test/namespaces/test_namespaces_in_builder_doc.rb
@@ -26,32 +26,33 @@ module Nokogiri
         e.namespace.nil? ? nil : e.namespace.href
       end
 
-      def test_default_ns
+      def test_builder_default_ns
         assert_equal 'ns:fruit', check_namespace(@doc.root)
       end
-      def test_parent_default_ns
+      def test_builder_parent_default_ns
         assert_equal 'ns:fruit', check_namespace(@doc.root.elements[0])
         assert_equal 'ns:fruit', check_namespace(@doc.root.elements[1])
       end
-      def test_grandparent_default_ns
+      def test_builder_grandparent_default_ns
         assert_equal 'ns:fruit', check_namespace(@doc.root.elements[0].elements[0])
       end
-      def test_parent_nondefault_ns
+      def test_builder_parent_nondefault_ns
         assert_equal 'ns:veg',   check_namespace(@doc.root.elements[2])
       end
-      def test_single_decl_ns_1
+      def test_builder_single_decl_ns_1
         assert_equal 'ns:dairy', check_namespace(@doc.root.elements[2].elements[0])
       end
-      def test_nondefault_attr_ns
-        assert_equal 'http://www.w3.org/1999/xlink', check_namespace(@doc.root.elements[2].elements[0].attribute_nodes[0])
+      def test_builder_nondefault_attr_ns
+        assert_equal 'http://www.w3.org/1999/xlink', 
+          check_namespace(@doc.root.elements[2].elements[0].attribute_nodes.find { |a| a.name =~ /href/ })
       end
-      def test_single_decl_ns_2
+      def test_builder_single_decl_ns_2
         assert_equal 'ns:meat',  check_namespace(@doc.root.elements[3])
       end
-      def test_buried_default_ns
+      def test_builder_buried_default_ns
         assert_equal 'ns:fruit',  check_namespace(@doc.root.elements[3].elements[0])
       end
-      def test_buried_decl_ns
+      def test_builder_buried_decl_ns
         assert_equal 'ns:veg',  check_namespace(@doc.root.elements[3].elements[1])
       end
     end
