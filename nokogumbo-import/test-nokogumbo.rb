adc0842e45b37f8f736373cb4b80a4d31a7fbb94
Sam Ruby <rubys@intertwingly.net>
2013-08-26 08:54:59 -0400

    cleanup tests

--- nokogumbo-import/test-nokogumbo.rb
+++ nokogumbo-import/test-nokogumbo.rb
@@ -4,46 +4,35 @@ require 'nokogumbo'
 require 'test/unit'
 
 class TestNokogumbo < Test::Unit::TestCase
-  def setup
-    @buffer = <<-EOF.gsub(/^    /, '')
-    <html>
-      <head>
-        <meta charset="utf-8"/>
-        <title>hello world</title>
-      </head>
-      <body>
-        <h1>hello world</h1>
-        <main>
-          <span>content</span>
-        </main>
-        <!-- test comment -->
-        <form>
-          <textarea>foo<x>bar</textarea>
-        </form>
-      </body>
-    </html>
-    EOF
-    @doc = Nokogiri::HTML5(@buffer)
-  end
-
   def test_element_text
-    assert_equal "content", @doc.at('span').text
+    doc = Nokogiri::HTML5(buffer)
+    assert_equal "content", doc.at('span').text
   end
 
   def test_element_cdata
-    assert_equal "foo<x>bar", @doc.at('textarea').text.strip
+    doc = Nokogiri::HTML5(buffer)
+    assert_equal "foo<x>bar", doc.at('textarea').text.strip
   end
 
   def test_attr_value
-    assert_equal "utf-8", @doc.at('meta')['charset']
+    doc = Nokogiri::HTML5(buffer)
+    assert_equal "utf-8", doc.at('meta')['charset']
   end
 
   def test_comment
-    assert_equal " test comment ", @doc.xpath('//comment()').text
+    doc = Nokogiri::HTML5(buffer)
+    assert_equal " test comment ", doc.xpath('//comment()').text
   end
 
   def test_unknown_element
-    assert_equal "main", @doc.at('main').name
+    doc = Nokogiri::HTML5(buffer)
+    assert_equal "main", doc.at('main').name
+  end
+
+  def test_IO
+    require 'stringio'
+    doc = Nokogiri::HTML5(StringIO.new(buffer))
+    assert_equal 'textarea', doc.at('form').element_children.first.name
   end
 
   if ''.respond_to? 'encoding'
@@ -71,4 +60,28 @@ class TestNokogumbo < Test::Unit::TestCase
     doc = Nokogumbo.parse("<!DOCTYPE html><html></html>")
     assert_match /<!DOCTYPE html>/, doc.to_html
   end
+
+private
+
+  def buffer
+    <<-EOF.gsub(/^      /, '')
+      <html>
+        <head>
+          <meta charset="utf-8"/>
+          <title>hello world</title>
+        </head>
+        <body>
+          <h1>hello world</h1>
+          <main>
+            <span>content</span>
+          </main>
+          <!-- test comment -->
+          <form>
+            <textarea>foo<x>bar</textarea>
+          </form>
+        </body>
+      </html>
+    EOF
+  end
+
 end
