f4c93dfc4379694a0f73b0d53fe34799cf38c579
Sam Ruby <rubys@intertwingly.net>
2013-09-01 18:24:57 -0400

    conditionally include libxml2

--- nokogumbo-import/extconf.rb
+++ nokogumbo-import/extconf.rb
@@ -1,22 +1,24 @@
 require 'mkmf'
-$CFLAGS = " -std=c99"
+$CFLAGS += " -std=c99"
 
-# libxml2 libraries from http://www.xmlsoft.org/
-pkg_config('libxml-2.0')
+if have_library('xml2', 'xmlNewDoc') 
+  # libxml2 libraries from http://www.xmlsoft.org/
+  pkg_config('libxml-2.0')
 
-# nokogiri configuration from gem install
-nokogiri_lib = Gem.find_files('nokogiri').
-  sort_by {|name| name[/nokogiri-([\d.]+)/,1].split('.').map(&:to_i)}.last
-if nokogiri_lib
-  nokogiri_ext = nokogiri_lib.sub(%r(lib/nokogiri(.rb)?$), 'ext/nokogiri')
+  # nokogiri configuration from gem install
+  nokogiri_lib = Gem.find_files('nokogiri').
+    sort_by {|name| name[/nokogiri-([\d.]+)/,1].split('.').map(&:to_i)}.last
+  if nokogiri_lib
+    nokogiri_ext = nokogiri_lib.sub(%r(lib/nokogiri(.rb)?$), 'ext/nokogiri')
 
-  # if that doesn't work, try workarounds found in Nokogiri's extconf
-  unless find_header('nokogiri.h', nokogiri_ext)
-    require "#{nokogiri_ext}/extconf.rb"
+    # if that doesn't work, try workarounds found in Nokogiri's extconf
+    unless find_header('nokogiri.h', nokogiri_ext)
+      require "#{nokogiri_ext}/extconf.rb"
+    end
+
+    # if found, enable direct calls to Nokogiri (and libxml2)
+    $CFLAGS += ' -DNGLIB' if find_header('nokogiri.h', nokogiri_ext)
   end
-
-  # if found, enable direct calls to Nokogiri (and libxml2)
-  $CFLAGS += ' -DNGLIB' if find_header('nokogiri.h', nokogiri_ext)
 end
 
 # add in gumbo-parser source from github if not already installed
