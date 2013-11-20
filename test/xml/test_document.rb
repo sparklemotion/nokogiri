sparklemotion/nokogiri

commit eebfda03a041f5dce2e73f1c7e09c8af61d686b5

Don't throw an exception on empty string io on JRuby. Fix #883.

lib/nokogiri/xml/document.rb
@@ -45,12 +45,13 @@ module Nokogiri
         # Give the options to the user
         yield options if block_given?
 
+        return new if empty_doc?(string_or_io)
+
         doc = if string_or_io.respond_to?(:read)
           url ||= string_or_io.respond_to?(:path) ? string_or_io.path : nil
           read_io(string_or_io, url, encoding, options.to_i)
         else
           # read_memory pukes on empty docs
-          return new if string_or_io.nil? or string_or_io.empty?
           read_memory(string_or_io, url, encoding, options.to_i)
         end
 
@@ -260,6 +261,12 @@ module Nokogiri
       end
 
       private
+      def self.empty_doc? string_or_io
+        string_or_io.nil? ||
+          (string_or_io.respond_to?(:empty?) && string_or_io.empty?) ||
+          (string_or_io.respond_to?(:eof?) && string_or_io.eof?)
+      end
+
       def implied_xpath_context
         "/"
       end

test/xml/test_document.rb
@@ -580,6 +580,11 @@ module Nokogiri
         assert set.length > 0
       end
 
+      def test_parsing_empty_io
+        doc = Nokogiri::XML.parse(StringIO.new(''))
+        refute_nil doc
+      end
+
       def test_search_on_empty_documents
         doc = Nokogiri::XML::Document.new
         ns = doc.search('//foo')

commit 2c9b7f1000b4870aa285e3c813f2de8c92593322

fix #1005. fix a regression introduced in eebfda0
Parsing empty documents should raise a SyntaxError when strict parsing
is turned on

lib/nokogiri/xml/document.rb
@@ -45,7 +45,7 @@ module Nokogiri
         # Give the options to the user
         yield options if block_given?
 
-        return new if empty_doc?(string_or_io)
+        return new if !options.strict? && empty_doc?(string_or_io)
 
         doc = if string_or_io.respond_to?(:read)
           url ||= string_or_io.respond_to?(:path) ? string_or_io.path : nil

test/xml/test_document.rb
@@ -26,6 +26,13 @@ module Nokogiri
         assert doc.root
       end
 
+      # issue #1005
+      def test_strict_parsing_empty_doc_should_raise_exception
+        assert_raises(SyntaxError) do
+          Nokogiri::XML(StringIO.new('')) { |c| c.strict }
+        end
+      end
+
       # issue #838
       def test_document_with_invalid_prolog
         doc = Nokogiri::XML '<? ?>'