sparklemotion/nokogiri

commit 89db31137f7f2b8f31dfc68e8d445fc801687037

Allow Nokogiri::XML::Node#parse from unparented non-element nodes.
Fixes #407

lib/nokogiri/xml/node.rb
@@ -508,6 +508,15 @@ module Nokogiri
       # *this* node.  Returns a XML::NodeSet containing the nodes parsed from
       # +string_or_io+.
       def parse string_or_io, options = nil
+        ##
+        # When the current node is unparented and not an element node, use the
+        # document as the parsing context instead. Otherwise, the in-context
+        # parser cannot find an element or a document node.
+        # Document Fragments are also not usable by the in-context parser.
+        if !element? && !xml? && (!parent || parent.fragment?)
+          return document.parse(string_or_io, options)
+        end
+
         options ||= (document.html? ? ParseOptions::DEFAULT_HTML : ParseOptions::DEFAULT_XML)
         if Fixnum === options
           options = Nokogiri::XML::ParseOptions.new(options)

test/xml/test_node.rb
@@ -133,6 +133,12 @@ module Nokogiri
         node.parse('<baz><</baz>')
       end
 
+      def test_parse_with_unparented_text_context_node
+        doc = XML::Document.new
+        elem = XML::Text.new("foo", doc)
+        elem.parse("<bar/>")
+      end
+
       def test_subclass_dup
         subclass = Class.new(Nokogiri::XML::Node)
         node = subclass.new('foo', @xml).dup

test/xml/test_document_fragment.rb
@@ -76,6 +76,11 @@ module Nokogiri
         assert_instance_of klass, doc
       end
 
+      def test_unparented_text_node_parse
+        fragment = Nokogiri::XML::DocumentFragment.parse("foo")
+        fragment.children.after("<bar/>")
+      end
+
       def test_xml_fragment
         fragment = Nokogiri::XML.fragment("<div>a</div>")
         assert_equal "<div>a</div>", fragment.to_s

commit 46fcdf9977d2abd8c1fce8eac43bed74c2c3eb58

Fixing Node#parse for HTML document nodes.
See related commit 89db31137f7f2b8f31dfc68e8d445fc801687037 which broke this functionality.

lib/nokogiri/xml/node.rb
@@ -513,7 +513,7 @@ module Nokogiri
         # document as the parsing context instead. Otherwise, the in-context
         # parser cannot find an element or a document node.
         # Document Fragments are also not usable by the in-context parser.
-        if !element? && !xml? && (!parent || parent.fragment?)
+        if !element? && !is_a?(Nokogiri::XML::Document) && (!parent || parent.fragment?)
           return document.parse(string_or_io, options)
         end
 

test/xml/test_node.rb
@@ -136,7 +136,29 @@ module Nokogiri
       def test_parse_with_unparented_text_context_node
         doc = XML::Document.new
         elem = XML::Text.new("foo", doc)
-        elem.parse("<bar/>")
+        x = elem.parse("<bar/>") # should not raise an exception
+        assert_equal x.first.name, "bar"
+      end
+
+      def test_parse_with_unparented_html_text_context_node
+        doc = HTML::Document.new
+        elem = XML::Text.new("div", doc)
+        x = elem.parse("<div/>") # should not raise an exception
+        assert_equal x.first.name, "div"
+      end
+
+      def test_parse_with_unparented_fragment_text_context_node
+        doc = XML::DocumentFragment.parse "<div><span>foo</span></div>"
+        elem = doc.at_css "span"
+        x = elem.parse("<span/>") # should not raise an exception
+        assert_equal x.first.name, "span"
+      end
+
+      def test_parse_with_unparented_html_fragment_text_context_node
+        doc = HTML::DocumentFragment.parse "<div><span>foo</span></div>"
+        elem = doc.at_css "span"
+        x = elem.parse("<span/>") # should not raise an exception
+        assert_equal x.first.name, "span"
       end
 
       def test_subclass_dup