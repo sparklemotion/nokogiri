e504c94cc9aa1bef440ffff2eb268a4a82462ee4
Akinori MUSHA <knu@idaemons.org>
2014-10-29 16:37:43 +0900

    Fix a test that does not pass with libxml2 >= 2.9.
    Unknown entity is not fatal any more in libxml2.

--- test/xml/test_entity_reference.rb
+++ test/xml/test_entity_reference.rb
@@ -236,9 +236,15 @@ EOF
         reader = Nokogiri::XML::Reader html, path do |cfg|
           cfg.default_xml
         end
-        assert_raises(Nokogiri::XML::SyntaxError) do
-          reader.each { |n| n }
+        if Nokogiri.uses_libxml? && Nokogiri::LIBXML_PARSER_VERSION.to_i >= 20900
+          # Unknown entity is not fatal in libxml2 >= 2.9
+          assert_equal 8, reader.count
+        else
+          assert_raises(Nokogiri::XML::SyntaxError) {
+            assert_equal 5, reader.count
+          }
         end
+        assert_operator reader.errors.size, :>, 0
       end
     end
   end
