41157f6342bdee0c88c5f522c5283016969c7c9b
Mike Dalessio <mike.dalessio@gmail.com>
2014-11-25 17:04:18 -0500

    Failing test exposing #canonicalize param issue
    as reported by #1189

--- test/xml/test_c14n.rb
+++ test/xml/test_c14n.rb
@@ -115,10 +115,12 @@ module Nokogiri
         doc2 = Nokogiri.XML <<-EOXML
 <n2:pdu xmlns:n1="http://example.com"
            xmlns:n2="http://foo.example"
+           xmlns:n4="http://foo.example"
            xml:lang="fr"
            xml:space="retain">
   <n1:elem2 xmlns:n1="http://example.net" xml:lang="en">
     <n3:stuff xmlns:n3="ftp://example.org"/>
+    <n4:stuff />
   </n1:elem2>
 </n2:pdu>        
         EOXML
@@ -128,25 +130,39 @@ module Nokogiri
     <n3:stuff></n3:stuff>
   </n1:elem2>', c14n
 
-        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize
-        assert_equal '<n1:elem2 xmlns:n1="http://example.net" xmlns:n2="http://foo.example" xml:lang="en" xml:space="retain">
+        expected = '<n1:elem2 xmlns:n1="http://example.net" xmlns:n2="http://foo.example" xmlns:n4="http://foo.example" xml:lang="en" xml:space="retain">
     <n3:stuff xmlns:n3="ftp://example.org"></n3:stuff>
-  </n1:elem2>', c14n
+    <n4:stuff></n4:stuff>
+  </n1:elem2>'
+        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize
+        assert_equal expected, c14n
 
-        excl_c14n = '<n1:elem2 xmlns:n1="http://example.net" xml:lang="en">
+        expected = '<n1:elem2 xmlns:n1="http://example.net" xml:lang="en">
     <n3:stuff xmlns:n3="ftp://example.org"></n3:stuff>
   </n1:elem2>'
         c14n = doc1.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize(XML::XML_C14N_EXCLUSIVE_1_0)
-        assert_equal excl_c14n, c14n
+        assert_equal expected, c14n
 
-        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize(XML::XML_C14N_EXCLUSIVE_1_0)
-        assert_equal excl_c14n, c14n
-
-        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize(XML::XML_C14N_EXCLUSIVE_1_0, ['n2'])
-        assert_equal '<n1:elem2 xmlns:n1="http://example.net" xmlns:n2="http://foo.example" xml:lang="en">
+        expected = '<n1:elem2 xmlns:n1="http://example.net" xml:lang="en">
     <n3:stuff xmlns:n3="ftp://example.org"></n3:stuff>
-  </n1:elem2>', c14n
+    <n4:stuff xmlns:n4="http://foo.example"></n4:stuff>
+  </n1:elem2>'
+        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize(XML::XML_C14N_EXCLUSIVE_1_0)
+        assert_equal expected, c14n
 
+        expected = '<n1:elem2 xmlns:n1="http://example.net" xmlns:n2="http://foo.example" xml:lang="en">
+    <n3:stuff xmlns:n3="ftp://example.org"></n3:stuff>
+    <n4:stuff xmlns:n4="http://foo.example"></n4:stuff>
+  </n1:elem2>'
+        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize(XML::XML_C14N_EXCLUSIVE_1_0, ['n2'])
+        assert_equal expected, c14n
+
+        expected = '<n1:elem2 xmlns:n1="http://example.net" xmlns:n2="http://foo.example" xmlns:n4="http://foo.example" xml:lang="en">
+    <n3:stuff xmlns:n3="ftp://example.org"></n3:stuff>
+    <n4:stuff></n4:stuff>
+  </n1:elem2>'
+        c14n = doc2.at_xpath('//n1:elem2', {'n1' => 'http://example.net'}).canonicalize(XML::XML_C14N_EXCLUSIVE_1_0, ['n2', 'n4'])
+        assert_equal expected, c14n
       end
 
       def test_wrong_params
