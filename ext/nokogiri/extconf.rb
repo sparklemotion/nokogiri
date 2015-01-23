sparklemotion/nokogiri

commit 0924a56d93e60a82cfc93eeb0559cbd45ba856ee

Restoring Ruby 1.9.2 support.
Closes #1207.

ext/nokogiri/extconf.rb
@@ -71,10 +71,19 @@ def do_clean
   exit! 0
 end
 
+def nokogiri_try_compile
+  args = if defined?(RUBY_VERSION) && RUBY_VERSION <= "1.9.2"
+           ["int main() {return 0;}"]
+         else
+           ["int main() {return 0;}", "", {werror: true}]
+         end
+end
+
+
 def add_cflags(flags)
   print "checking if the C compiler accepts #{flags}... "
   with_cflags("#{$CFLAGS} #{flags}") do
-    if try_compile("int main() {return 0;}", '', werror: true)
+    if nokogiri_try_compile
       puts 'yes'
       true
     else

commit 5f8fa1795b312ae3ca894b7a58e1aba3d31bce4e

Fix try_compile in extconf.rb
because I broke it in 0924a56 when trying to refactor. *headdesk*

ext/nokogiri/extconf.rb
@@ -77,6 +77,7 @@ def nokogiri_try_compile
          else
            ["int main() {return 0;}", "", {werror: true}]
          end
+  try_compile(*args)
 end
 
 