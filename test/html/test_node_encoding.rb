6ec01b69bcddf6853563915ffeed2af8ba23d2d3
Christian Meier <m.kristian@web.de>
2015-02-21 11:23:12 +0100

    parsing numeric character references needs an update of nekohtml
    the new version of nekohtml brought a few regressions. this commit fixes
    but two error warning ones.
    
    it avoids to autocomplete the tbody tag around tr tags of a table. the check
    of unknown html did change upstream and got adjusted.
    
    fixes #1113
    
    Sponsored by Lookout Inc.

--- test/html/test_node_encoding.rb
+++ test/html/test_node_encoding.rb
@@ -20,6 +20,25 @@ module Nokogiri
         contents = doc.at('h2').inner_html
         assert_match hello, contents
       end
+
+      def test_encoding_GH_1113
+        doc = Nokogiri::HTML::Document.new
+        hex = '<p>&#x1f340;</p>'
+        decimal = '<p>&#127808;</p>'
+        encoded = '<p>🍀</p>'
+
+        doc.encoding = 'UTF-8'
+        expected = encoded
+        assert_equal expected, doc.fragment(hex).to_s
+        assert_equal expected, doc.fragment(decimal).to_s
+        assert_equal expected, doc.fragment(encoded).to_s
+
+        doc.encoding = 'US-ASCII'
+        expected = defined?(JRUBY_VERSION) ? hex : decimal
+        assert_equal expected, doc.fragment(hex).to_s
+        assert_equal expected, doc.fragment(decimal).to_s
+        assert_equal expected, doc.fragment(encoded).to_s
+      end
     end
   end
 end
