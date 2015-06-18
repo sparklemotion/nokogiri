be349380e2523a08a3b03158dab1045bd4c1d674
Laurent Arnoud <laurent@spkdev.net>
2015-06-18 23:23:58 +0200

    Add line column to message
    Useful for `map(&:to_s)` and simple output

--- test/xml/test_syntax_error.rb
+++ test/xml/test_syntax_error.rb
@@ -25,6 +25,14 @@ module Nokogiri
           reader.read
         }
       end unless Nokogiri.jruby? # which does not internally call `errors`
+
+      def test_line_column
+        bad_doc = Nokogiri::XML('test')
+        error = bad_doc.errors.first
+        assert_equal "Start tag expected, '<' not found, 1, 1", error.message
+        assert_equal 1, error.line
+        assert_equal 1, error.column
+      end
     end
   end
 end
