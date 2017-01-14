ed14aa2b79e294bcc416efb2aa055ca35d4f5267
Mike Dalessio <mike.dalessio@gmail.com>
2017-01-14 12:41:12 -0500

    VersionInfo#loaded_parser_version more readable

--- lib/nokogiri/version.rb
+++ lib/nokogiri/version.rb
@@ -12,9 +12,10 @@ module Nokogiri
     end
 
     def loaded_parser_version
-      LIBXML_PARSER_VERSION.scan(/^(\d+)(\d\d)(\d\d)(?!\d)/).first.collect{ |j|
-        j.to_i
-      }.join(".")
+      LIBXML_PARSER_VERSION.
+        scan(/^(\d+)(\d\d)(\d\d)(?!\d)/).first.
+        collect(&:to_i).
+        join(".")
     end
 
     def compiled_parser_version
