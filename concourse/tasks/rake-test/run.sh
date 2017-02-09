42f8ac0af5d991536571f76435d7ffe910a1c54e
Mike Dalessio <mike.dalessio@gmail.com>
2017-02-09 17:58:51 -0500

    concourse: refactor rake-test/run.sh to pushd
    so we can (in the next commit) get the PR builds to use master for CI
    config, but the PR branch as the subject-under-test.

--- concourse/tasks/rake-test/run.sh
+++ concourse/tasks/rake-test/run.sh
@@ -2,56 +2,60 @@
 
 set -e -x
 
-VERSION_INFO=$(ruby -v)
-RUBY_ENGINE=$(cut -d" " -f1 <<< "${VERSION_INFO}")
-RUBY_VERSION=$(cut -d" " -f2 <<< "${VERSION_INFO}")
+pushd nokogiri
 
-APT_UPDATED=false
+  VERSION_INFO=$(ruby -v)
+  RUBY_ENGINE=$(cut -d" " -f1 <<< "${VERSION_INFO}")
+  RUBY_VERSION=$(cut -d" " -f2 <<< "${VERSION_INFO}")
 
-function mri-24-or-greater {
-  if [[ $RUBY_ENGINE != "ruby" ]] ; then
-    return 1
-  fi
+  APT_UPDATED=false
 
-  if echo $RUBY_VERSION | grep "^[0-2]\.[0-3]\." > /dev/null ; then
-    return 1
-  fi
+  function mri-24-or-greater {
+    if [[ $RUBY_ENGINE != "ruby" ]] ; then
+      return 1
+    fi
 
-  return 0
-}
+    if echo $RUBY_VERSION | grep "^[0-2]\.[0-3]\." > /dev/null ; then
+      return 1
+    fi
 
-function rbx-engine {
-  if [[ $RUBY_ENGINE == "rubinius" ]] ; then
     return 0
+  }
+
+  function rbx-engine {
+    if [[ $RUBY_ENGINE == "rubinius" ]] ; then
+      return 0
+    fi
+    return 1
+  }
+
+  function ensure-apt-update {
+    if [[ $APT_UPDATED != "false" ]] ; then
+      return
+    fi
+
+    apt-get update
+    APT_UPDATED=true
+  }
+
+  if mri-24-or-greater ; then
+    export RUBYOPT="--enable-frozen-string-literal --debug=frozen-string-literal"
   fi
-  return 1
-}
 
-function ensure-apt-update {
-  if [[ $APT_UPDATED != "false" ]] ; then
-    return
+  if rbx-engine ; then
+    ensure-apt-update
+    apt-get install -y ca-certificates gcc pkg-config libxml2-dev libxslt-dev
   fi
 
-  apt-get update
-  APT_UPDATED=true
-}
+  RAKE_TASK="test"
 
-if mri-24-or-greater ; then
-  export RUBYOPT="--enable-frozen-string-literal --debug=frozen-string-literal"
-fi
+  if [[ $TEST_WITH_VALGRIND != "" ]] ; then
+    RAKE_TASK="test:valgrind" # override
+    ensure-apt-update
+    apt-get install -y valgrind
+  fi
 
-if rbx-engine ; then
-  ensure-apt-update
-  apt-get install -y ca-certificates gcc pkg-config libxml2-dev libxslt-dev
-fi
+  bundle install
+  bundle exec rake ${RAKE_TASK}
 
-RAKE_TASK="test"
-
-if [[ $TEST_WITH_VALGRIND != "" ]] ; then
-  RAKE_TASK="test:valgrind" # override
-  ensure-apt-update
-  apt-get install -y valgrind
-fi
-
-bundle install
-bundle exec rake ${RAKE_TASK}
+popd
