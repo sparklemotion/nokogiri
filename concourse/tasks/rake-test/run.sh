1d28623c6fb998c53df5037e9d7e7c98e4c4ed35
Mike Dalessio <mike.dalessio@gmail.com>
2017-02-09 18:47:02 -0500

    concourse: run.sh prelude before the pushd
    this helps visual scanning of the script, and makes it clear what
    depends on descending into the project directory.

--- concourse/tasks/rake-test/run.sh
+++ concourse/tasks/rake-test/run.sh
@@ -2,50 +2,50 @@
 
 set -e -x
 
-pushd nokogiri
+VERSION_INFO=$(ruby -v)
+RUBY_ENGINE=$(cut -d" " -f1 <<< "${VERSION_INFO}")
+RUBY_VERSION=$(cut -d" " -f2 <<< "${VERSION_INFO}")
 
-  VERSION_INFO=$(ruby -v)
-  RUBY_ENGINE=$(cut -d" " -f1 <<< "${VERSION_INFO}")
-  RUBY_VERSION=$(cut -d" " -f2 <<< "${VERSION_INFO}")
+APT_UPDATED=false
 
-  APT_UPDATED=false
+FROZEN_STRING_REF="53f9b66"
 
-  FROZEN_STRING_REF="53f9b66"
+function mri-24-or-greater {
+  if [[ $RUBY_ENGINE != "ruby" ]] ; then
+    return 1
+  fi
 
-  function mri-24-or-greater {
-    if [[ $RUBY_ENGINE != "ruby" ]] ; then
-      return 1
-    fi
+  if echo $RUBY_VERSION | grep "^[0-2]\.[0-3]\." > /dev/null ; then
+    return 1
+  fi
 
-    if echo $RUBY_VERSION | grep "^[0-2]\.[0-3]\." > /dev/null ; then
-      return 1
-    fi
+  return 0
+}
 
+function commit-is-post-frozen-string-support {
+  if git merge-base --is-ancestor ${FROZEN_STRING_REF} HEAD ; then
     return 0
-  }
+  fi
+  return 1
+}
 
-  function commit-is-post-frozen-string-support {
-    if git merge-base --is-ancestor ${FROZEN_STRING_REF} HEAD ; then
-       return 0
-    fi
-    return 1
-  }
+function rbx-engine {
+  if [[ $RUBY_ENGINE == "rubinius" ]] ; then
+    return 0
+  fi
+  return 1
+}
 
-  function rbx-engine {
-    if [[ $RUBY_ENGINE == "rubinius" ]] ; then
-      return 0
-    fi
-    return 1
-  }
+function ensure-apt-update {
+  if [[ $APT_UPDATED != "false" ]] ; then
+    return
+  fi
 
-  function ensure-apt-update {
-    if [[ $APT_UPDATED != "false" ]] ; then
-      return
-    fi
+  apt-get update
+  APT_UPDATED=true
+}
 
-    apt-get update
-    APT_UPDATED=true
-  }
+pushd nokogiri
 
   if mri-24-or-greater && commit-is-post-frozen-string-support ; then
     export RUBYOPT="--enable-frozen-string-literal --debug=frozen-string-literal"
