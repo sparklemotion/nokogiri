76b41e8d23482fd961e96b138afdbbadbb98586a
Mike Dalessio <mike.dalessio@gmail.com>
2017-02-10 01:14:49 -0500

    concourse: windows tasks use ci resource
    [ci skip]

--- concourse/windows/rake-test.ps1
+++ concourse/windows/rake-test.ps1
@@ -1,8 +1,12 @@
-. "concourse\windows\common.ps1"
+. "ci\concourse\windows\common.ps1"
 
 prepend-path $ruby23_bin_path
 $env:RUBYOPT = "-rdevkit"
 
-stream-cmd "gem" "install bundler"
-stream-cmd "bundle" "install"
-stream-cmd "bundle" "exec rake test"
+push-location nokogiri
+
+    stream-cmd "gem" "install bundler"
+    stream-cmd "bundle" "install"
+    stream-cmd "bundle" "exec rake test"
+
+pop-location
