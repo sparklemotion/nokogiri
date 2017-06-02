f1f341cd6d75592cf51d18d81dc8ae6014fd2b51
Sam Ruby <rubys@intertwingly.net>
2017-06-02 14:35:09 -0400

    Revert "Fix nokogiri rpath build."
    This reverts commit b4359b19a8d3a5f3f6a6b2ed742797d0fed68ef2.

--- nokogumbo-import/ext/nokogumboc/extconf.rb
+++ nokogumbo-import/ext/nokogumboc/extconf.rb
@@ -1,18 +1,16 @@
 require 'mkmf'
-$CFLAGS += ' -std=c99'
+$CFLAGS += " -std=c99"
 
-if have_library('xml2', 'xmlNewDoc')
+if have_library('xml2', 'xmlNewDoc') 
   # libxml2 libraries from http://www.xmlsoft.org/
   pkg_config('libxml-2.0')
 
   # nokogiri configuration from gem install
-  nokogiri_lib = Gem
-                 .find_files('nokogiri')
-                 .select { |name| name.match(%r{gems/nokogiri-([\d.]+)/lib/nokogiri}) }
-                 .sort_by { |name| name[/nokogiri-([\d.]+)/, 1].split('.').map(&:to_i) }
-                 .last
+  nokogiri_lib = Gem.find_files('nokogiri').
+    select { |name| name.match(%r{gems/nokogiri-([\d.]+)/lib/nokogiri}) }.
+    sort_by {|name| name[/nokogiri-([\d.]+)/,1].split('.').map(&:to_i)}.last
   if nokogiri_lib
-    nokogiri_ext = nokogiri_lib.sub(%r{lib/nokogiri(.rb)?$}, 'ext/nokogiri')
+    nokogiri_ext = nokogiri_lib.sub(%r(lib/nokogiri(.rb)?$), 'ext/nokogiri')
 
     # if that doesn't work, try workarounds found in Nokogiri's extconf
     unless find_header('nokogiri.h', nokogiri_ext)
@@ -21,28 +19,6 @@ if have_library('xml2', 'xmlNewDoc')
 
     # if found, enable direct calls to Nokogiri (and libxml2)
     $CFLAGS += ' -DNGLIB' if find_header('nokogiri.h', nokogiri_ext)
-
-    # If libnokogiri is not on the build path, we need to add it.
-    unless have_library('nokogiri', 'Nokogiri_wrap_xml_document')
-      nokogiri_libfile = 'nokogiri.' + RbConfig::CONFIG['DLEXT']
-      nokogiri_libpath = File.join(nokogiri_ext, nokogiri_libfile)
-      if File.exist? nokogiri_libpath
-        $LDFLAGS += " -Wl,-rpath #{nokogiri_ext} -L#{nokogiri_ext} "
-        # GNU ld:
-        # '-lFOO' => looks for a file named 'libFOO.a' or 'libFOO.so*'
-        # '-l:FOO' => looks for a file named exactly 'FOO'
-        # OSX/Mach ld:
-        # - '-l:' is not supported.
-        # - Only links '.dylib', not '.bundle'
-        #
-        # Nokogiri does NOT have a lib prefix, so we need to be creative in
-        # testing; and might have either .dylib or .bundle suffixes.
-        $LIBS += ' ' + nokogiri_libpath + ' ' \
-          if %w(so dylib).include? RbConfig::CONFIG['DLEXT']
-      else
-        puts 'WARNING! Could not find Nokogiri_wrap_xml_document symbol; build might fail.'
-      end
-    end
   end
 end
 
@@ -52,12 +28,12 @@ unless have_library('gumbo', 'gumbo_parse')
   unless File.exist? "#{rakehome}/ext/nokogumboc/gumbo.h"
     require 'fileutils'
     FileUtils.cp Dir["#{rakehome}/gumbo-parser/src/*"],
-                 "#{rakehome}/ext/nokogumboc"
+      "#{rakehome}/ext/nokogumboc"
 
     case RbConfig::CONFIG['target_os']
     when 'mingw32', /mswin/
       FileUtils.cp Dir["#{rakehome}/gumbo-parser/visualc/include/*"],
-                   "#{rakehome}/ext/nokogumboc"
+        "#{rakehome}/ext/nokogumboc"
     end
 
     $srcs = $objs = nil
