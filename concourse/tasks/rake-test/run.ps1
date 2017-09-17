9fea7e8771c1dcdbc6e8612658b623784d0c185f
Mike Dalessio <mike.dalessio@gmail.com>
2017-09-17 15:52:40 -0400

    use ruby from windows-ruby-dev-tools-release

--- concourse/tasks/rake-test/run.ps1
+++ concourse/tasks/rake-test/run.ps1
@@ -1,13 +1,11 @@
-. "ci\concourse\shared\common.ps1"
 . "c:\var\vcap\packages\windows-ruby-dev-tools\prelude.ps1"
 
-prepend-path $ruby23_bin_path
 $env:RUBYOPT = "-rdevkit"
 
 push-location nokogiri
 
-    stream-cmd "gem" "install bundler"
-    stream-cmd "bundle" "install"
-    stream-cmd "bundle" "exec rake test"
+    system-cmd "gem install bundler"
+    system-cmd "bundle install"
+    system-cmd "bundle exec rake test"
 
 pop-location
