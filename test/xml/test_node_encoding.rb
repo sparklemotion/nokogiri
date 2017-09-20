sparklemotion/nokogiri

commit 53f9b66ee60342a639ff1c1e60b75558ea3be4ac

Work with ruby 2.3's --enable-frozen-string-literal
This is an extension to PR #1412 .

lib/nokogiri/xml/node.rb
@@ -623,7 +623,7 @@ module Nokogiri
         encoding = options[:encoding] || document.encoding
         options[:encoding] = encoding
 
-        outstring = ""
+        outstring = String.new
         if encoding && outstring.respond_to?(:force_encoding)
           outstring.force_encoding(Encoding.find(encoding))
         end

ext/nokogiri/extconf.rb
@@ -223,8 +223,8 @@ def iconv_configure_flags
 
       return [
         '--with-iconv=yes',
-        *("CPPFLAGS=#{idirs.map { |dir| '-I' << dir }.join(' ')}" if idirs),
-        *("LDFLAGS=#{ldirs.map { |dir| '-L' << dir }.join(' ')}" if ldirs),
+        *("CPPFLAGS=#{idirs.map { |dir| '-I' + dir }.join(' ')}" if idirs),
+        *("LDFLAGS=#{ldirs.map { |dir| '-L' + dir }.join(' ')}" if ldirs),
       ]
     end
   end
@@ -307,7 +307,7 @@ def process_recipe(name, version, static_p, cross_p)
     if RbConfig::CONFIG['target_cpu'] == 'universal'
       %w[CFLAGS LDFLAGS].each do |key|
         unless env[key].include?('-arch')
-          env[key] << ' ' << RbConfig::CONFIG['ARCH_FLAG']
+          env[key] += ' ' + RbConfig::CONFIG['ARCH_FLAG']
         end
       end
     end

lib/nokogiri/xml/sax/parser.rb
@@ -68,8 +68,7 @@ module Nokogiri
 
         # Create a new Parser with +doc+ and +encoding+
         def initialize doc = Nokogiri::XML::SAX::Document.new, encoding = 'UTF-8'
-          check_encoding(encoding)
-          @encoding = encoding
+          @encoding = check_encoding(encoding)
           @document = doc
           @warned   = false
         end
@@ -88,9 +87,8 @@ module Nokogiri
         ###
         # Parse given +io+
         def parse_io io, encoding = 'ASCII'
-          check_encoding(encoding)
-          @encoding = encoding
-          ctx = ParserContext.io(io, ENCODINGS[encoding])
+          @encoding = check_encoding(encoding)
+          ctx = ParserContext.io(io, ENCODINGS[@encoding])
           yield ctx if block_given?
           ctx.parse_with self
         end
@@ -114,8 +112,9 @@ module Nokogiri
 
         private
         def check_encoding(encoding)
-          encoding.upcase!
-          raise ArgumentError.new("'#{encoding}' is not a valid encoding") unless ENCODINGS[encoding]
+          encoding.upcase.tap do |enc|
+            raise ArgumentError.new("'#{enc}' is not a valid encoding") unless ENCODINGS[enc]
+          end
         end
       end
     end

test/html/test_document.rb
@@ -15,7 +15,7 @@ module Nokogiri
       end
 
       def test_does_not_fail_with_illformatted_html
-        doc = Nokogiri::HTML('"</html>";'.force_encoding(Encoding::BINARY))
+        doc = Nokogiri::HTML('"</html>";'.dup.force_encoding(Encoding::BINARY))
         assert_not_nil doc
       end
 

test/xml/test_document.rb
@@ -221,7 +221,7 @@ module Nokogiri
       end
 
       def test_pp
-        out = StringIO.new('')
+        out = StringIO.new(String.new)
         ::PP.pp @xml, out
         assert_operator out.string.length, :>, 0
       end

commit 076a7414e799ac3d2bacd0c78c6ca6f7a8a6265e

Use UTF-8 as default encoding for Node#serialize
Node#serialize used to return UTF-8 if no encoding was given.
However this got broken in commit 53f9b66ee6.

Since UTF-8 is the default in XML and HTML5 specs, it makes sense to
use UTF-8 in serialize as well and enforce this in the tests.

Fixes #1659

lib/nokogiri/xml/node.rb
@@ -624,9 +624,7 @@ module Nokogiri
         options[:encoding] = encoding
 
         outstring = String.new
-        if encoding && outstring.respond_to?(:force_encoding)
-          outstring.force_encoding(Encoding.find(encoding))
-        end
+        outstring.force_encoding(Encoding.find(encoding || 'utf-8'))
         io = StringIO.new(outstring)
         write_to io, options, &block
         io.string

test/html/test_node_encoding.rb
@@ -26,6 +26,12 @@ module Nokogiri
         assert_equal @html.serialize, @doc.serialize
       end
 
+      def test_default_encoding
+        doc = Nokogiri::HTML(nil)
+        assert_nil doc.encoding
+        assert_equal 'UTF-8', doc.serialize.encoding.name
+      end
+
       def test_encode_special_chars
         foo = @html.css('a').first.encode_special_chars('foo')
         assert_equal 'UTF-8', foo.encoding.name

test/xml/test_node_encoding.rb
@@ -13,6 +13,12 @@ module Nokogiri
         assert_equal @xml.serialize, @doc.serialize
       end
 
+      def test_default_encoding
+        doc = Nokogiri::XML(VEHICLE_XML)
+        assert_nil doc.encoding
+        assert_equal 'UTF-8', doc.serialize.encoding.name
+      end
+
       def test_encoding_GH_1113
         utf8 = '<frag>shahid ὡ 𐄣 𢂁</frag>'
         hex = '<frag>shahid &#x1f61; &#x10123; &#x22081;</frag>'