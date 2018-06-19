08677fcab881dc8ddf30660d44caf375a5c31720
Craig Barnes <Cr@igBarn.es>
2018-06-19 23:00:00 +0100

    Compile lib/foreign_attrs.c with "gperf -n"
    This tweak allows gperf to omit 7 blank elements from the table,
    thereby making it a minimal, perfect hash table.

--- nokogumbo-import/gumbo-parser/src/foreign_attrs.c
+++ nokogumbo-import/gumbo-parser/src/foreign_attrs.c
@@ -1,6 +1,6 @@
 /* ANSI-C code produced by gperf version 3.1 */
-/* Command-line: gperf -m100 lib/foreign_attrs.gperf  */
-/* Computed positions: -k'7' */
+/* Command-line: gperf -m100 -n lib/foreign_attrs.gperf  */
+/* Computed positions: -k'2,8' */
 /* Filtered by: mk/gperf-filter.sed */
 
 #include "replacement.h"
@@ -10,51 +10,56 @@
 #define TOTAL_KEYWORDS 11
 #define MIN_WORD_LENGTH 5
 #define MAX_WORD_LENGTH 13
-#define MIN_HASH_VALUE 5
-#define MAX_HASH_VALUE 17
-/* maximum key range = 13, duplicates = 0 */
+#define MIN_HASH_VALUE 0
+#define MAX_HASH_VALUE 10
+/* maximum key range = 11, duplicates = 0 */
 
 static inline unsigned int
 hash (register const char *str, register size_t len)
 {
   static const unsigned char asso_values[] =
     {
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18,  0, 18, 18,
-      18, 18, 18, 18,  7, 18, 18, 18, 18, 18,
-       0, 18, 18, 18,  6,  4,  0, 18, 18, 18,
-       4, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18, 18, 18, 18, 18,
-      18, 18, 18, 18, 18, 18
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11,  2,
+      11, 10, 11,  9,  7,  6, 11, 11,  1,  0,
+      11,  5, 11, 11,  4, 11, 11, 11, 11, 11,
+      11,  3, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+      11, 11, 11, 11, 11, 11
     };
-  register unsigned int hval = len;
+  register unsigned int hval = 0;
 
-  switch (hval)
+  switch (len)
     {
       default:
-        hval += asso_values[(unsigned char)str[6]];
+        hval += asso_values[(unsigned char)str[7]];
       /*FALLTHROUGH*/
+      case 7:
       case 6:
       case 5:
+      case 4:
+      case 3:
+      case 2:
+        hval += asso_values[(unsigned char)str[1]];
         break;
     }
   return hval;
@@ -65,25 +70,21 @@ gumbo_get_foreign_attr_replacement (register const char *str, register size_t le
 {
   static const unsigned char lengthtable[] =
     {
-       0,  0,  0,  0,  0,  5,  0,  0,  8,  9, 10, 11,  8, 13,
-      10, 11, 10, 10
+       5, 11,  9, 13, 10, 10, 10, 11, 10,  8,  8
     };
   static const ForeignAttrReplacement wordlist[] =
     {
-      {(char*)0}, {(char*)0}, {(char*)0}, {(char*)0},
-      {(char*)0},
       {"xmlns", "xmlns", GUMBO_ATTR_NAMESPACE_XMLNS},
-      {(char*)0}, {(char*)0},
-      {"xml:lang", "lang", GUMBO_ATTR_NAMESPACE_XML},
-      {"xml:space", "space", GUMBO_ATTR_NAMESPACE_XML},
-      {"xlink:type", "type", GUMBO_ATTR_NAMESPACE_XLINK},
-      {"xlink:title", "title", GUMBO_ATTR_NAMESPACE_XLINK},
-      {"xml:base", "base", GUMBO_ATTR_NAMESPACE_XML},
-      {"xlink:actuate", "actuate", GUMBO_ATTR_NAMESPACE_XLINK},
-      {"xlink:show", "show", GUMBO_ATTR_NAMESPACE_XLINK},
       {"xmlns:xlink", "xlink", GUMBO_ATTR_NAMESPACE_XMLNS},
+      {"xml:space", "space", GUMBO_ATTR_NAMESPACE_XML},
+      {"xlink:actuate", "actuate", GUMBO_ATTR_NAMESPACE_XLINK},
+      {"xlink:type", "type", GUMBO_ATTR_NAMESPACE_XLINK},
+      {"xlink:href", "href", GUMBO_ATTR_NAMESPACE_XLINK},
       {"xlink:role", "role", GUMBO_ATTR_NAMESPACE_XLINK},
-      {"xlink:href", "href", GUMBO_ATTR_NAMESPACE_XLINK}
+      {"xlink:title", "title", GUMBO_ATTR_NAMESPACE_XLINK},
+      {"xlink:show", "show", GUMBO_ATTR_NAMESPACE_XLINK},
+      {"xml:lang", "lang", GUMBO_ATTR_NAMESPACE_XML},
+      {"xml:base", "base", GUMBO_ATTR_NAMESPACE_XML}
     };
 
   if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
