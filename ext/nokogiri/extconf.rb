sparklemotion/nokogiri

commit 77be60c3681f76db0416d98da2e940a10bd1169d

tweak compiler flags to reduce noise
remove a few that generate lots of warnings on ruby.h:
- -Wcast-qual
- -Wconversion

add -Wextra back (it was removed in 2008)

ext/nokogiri/extconf.rb
@@ -434,7 +434,7 @@ end
 
 if RbConfig::MAKEFILE_CONFIG['CC'] =~ /gcc/
   $CFLAGS << " -O3" unless $CFLAGS[/-O\d/]
-  $CFLAGS << " -Wall -Wcast-qual -Wwrite-strings -Wconversion -Wmissing-noreturn -Winline"
+  $CFLAGS << " -Wall -Wextra -Wwrite-strings -Wmissing-noreturn -Winline"
 end
 
 case

commit b3750eb71e101287aa0e7a231232222c7213b3f3

remove `-Wextra` CFLAG
fixes #1772 which was a regression from 77be60c

ext/nokogiri/extconf.rb
@@ -434,7 +434,7 @@ end
 
 if RbConfig::MAKEFILE_CONFIG['CC'] =~ /gcc/
   $CFLAGS << " -O3" unless $CFLAGS[/-O\d/]
-  $CFLAGS << " -Wall -Wcast-qual -Wwrite-strings -Wextra -Wmissing-noreturn -Winline"
+  $CFLAGS << " -Wall -Wcast-qual -Wwrite-strings -Wmissing-noreturn -Winline"
 end
 
 case