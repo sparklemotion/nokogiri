sparklemotion/nokogiri

commit f2f3a3bf2cb36865c681b51418dbf7957f00a1c9

DocumentFragment#dup avoids unnecessary copies.
[fixes #1063]

lib/nokogiri/xml/document_fragment.rb
@@ -25,6 +25,15 @@ module Nokogiri
         children.each { |child| child.parent = self }
       end
 
+      def dup
+        new_document = document.dup
+        new_fragment = XML::DocumentFragment.new(new_document)
+        children.each do |child|
+          child.dup(1, new_document).parent = new_fragment
+        end
+        new_fragment
+      end
+
       ###
       # return the name for DocumentFragment
       def name

test/xml/test_document_fragment.rb
@@ -263,6 +263,20 @@ EOS
         Nokogiri::XML::DocumentFragment.parse(input) # assert_nothing_raised
       end
 
+      def test_dup_creates_tree_with_identical_structure
+        original = Nokogiri::XML::DocumentFragment.parse("<div><p>hello</p></div>")
+        duplicate = original.dup
+        assert_equal original.to_html, duplicate.to_html
+      end
+
+      def test_dup_creates_mutable_tree
+        original = Nokogiri::XML::DocumentFragment.parse("<div><p>hello</p></div>")
+        duplicate = original.dup
+        duplicate.at_css("div").add_child("<b>hello there</b>")
+        assert_nil original.at_css("b")
+        assert_not_nil duplicate.at_css("b")
+      end
+
       if Nokogiri.uses_libxml?
         def test_for_libxml_in_context_fragment_parsing_bug_workaround
           10.times do

commit e9ac29275afde6f670a4db64d609a7a07c828ea6

Fix XML::DocumentFragment to return an instance of callee's class
Fixes #1846. Bug introduced in f2f3a3b.

lib/nokogiri/xml/document_fragment.rb
@@ -28,7 +28,7 @@ module Nokogiri
       if Nokogiri.uses_libxml?
         def dup
           new_document = document.dup
-          new_fragment = XML::DocumentFragment.new(new_document)
+          new_fragment = self.class.new(new_document)
           children.each do |child|
             child.dup(1, new_document).parent = new_fragment
           end

test/html/test_document_fragment.rb
@@ -305,6 +305,13 @@ module Nokogiri
         assert_equal original_errors1, frag1.errors
         assert_equal original_errors2, frag2.errors
       end
+
+      def test_dup_should_create_an_html_document_fragment
+        # https://github.com/sparklemotion/nokogiri/issues/1846
+        original = Nokogiri::HTML::DocumentFragment.parse("<div><p>hello</p></div>")
+        duplicate = original.dup
+        assert_instance_of Nokogiri::HTML::DocumentFragment, duplicate
+      end
     end
   end
 end

test/xml/test_document_fragment.rb
@@ -263,6 +263,20 @@ EOS
         Nokogiri::XML::DocumentFragment.parse(input) # assert_nothing_raised
       end
 
+      def test_dup_should_exist_in_a_new_document
+        # https://github.com/sparklemotion/nokogiri/issues/1063
+        original = Nokogiri::XML::DocumentFragment.parse("<div><p>hello</p></div>")
+        duplicate = original.dup
+        assert_not_equal original.document, duplicate.document
+      end
+
+      def test_dup_should_create_an_xml_document_fragment
+        # https://github.com/sparklemotion/nokogiri/issues/1846
+        original = Nokogiri::XML::DocumentFragment.parse("<div><p>hello</p></div>")
+        duplicate = original.dup
+        assert_instance_of Nokogiri::XML::DocumentFragment, duplicate
+      end
+
       def test_dup_creates_tree_with_identical_structure
         original = Nokogiri::XML::DocumentFragment.parse("<div><p>hello</p></div>")
         duplicate = original.dup