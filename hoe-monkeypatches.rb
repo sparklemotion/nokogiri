95d84a8f5d5a4338b1c6a79dd383d740ce5cb224
Mike Dalessio <mike.dalessio@gmail.com>
2020-02-10 08:16:21 -0500

    dev: work around hoe deprecation warning

--- hoe-monkeypatches.rb
+++ hoe-monkeypatches.rb
@@ -1,3 +1,13 @@
 #
-#  this file currently empty
+#  this patch works around https://github.com/seattlerb/hoe/issues/103
 #
+require "hoe"
+
+class Hoe
+  alias_method :old_parse_urls, :parse_urls
+
+  def parse_urls(text)
+    return urls unless urls.empty?
+    old_parse_urls(text)
+  end
+end
