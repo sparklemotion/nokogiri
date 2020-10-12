fa81190a0b29e3c60bbaacc41df9305ef20c328c
Mike Dalessio <mike.dalessio@gmail.com>
2020-10-12 13:03:12 -0400

    fix: xml_node_set.c: upstream truffleruby function signature patch
    originally from https://github.com/oracle/truffleruby/blob/master/lib/truffle/truffle/patches/nokogiri_patches.rb#L29-L36
    
    Part of #1882

--- ext/nokogiri/xml_node_set.c
+++ ext/nokogiri/xml_node_set.c
@@ -319,7 +319,7 @@ static VALUE slice(int argc, VALUE *argv, VALUE self)
  *
  * Return this list as an Array
  */
-static VALUE to_array(VALUE self, VALUE rb_node)
+static VALUE to_array(VALUE self)
 {
   xmlNodeSetPtr node_set ;
   VALUE list;
