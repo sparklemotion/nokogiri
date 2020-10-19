19489f173dd690e3a429f8c344fe1f19dd7f436c
Mike Dalessio <mike.dalessio@gmail.com>
2020-10-19 18:10:36 -0400

    cleanup(extconf): comment and refactor package_config

--- ext/nokogiri/extconf.rb
+++ ext/nokogiri/extconf.rb
@@ -125,27 +125,37 @@ def do_clean
 end
 
 def package_config pkg, options={}
+  # use MakeMakefile#pkg_config, which uses the system utility `pkg-config`.
   package = pkg_config(pkg)
   return package if package
 
+  # only if that utility isn't found do we fall back to the pkg-config gem, and
+  # wrap it with the same logic as MakeMakefile#pkg_config
   begin
     require 'rubygems'
     gem 'pkg-config', (gem_ver='~> 1.1')
     require 'pkg-config' and message("Using pkg-config gem version #{PKGConfig::VERSION}\n")
   rescue LoadError
-    message "pkg-config could not be used to find #{pkg}\nPlease install either `pkg-config` or the pkg-config gem per\n\n    gem install pkg-config -v #{gem_ver.inspect}\n\n"
-  else
-    return nil unless PKGConfig.have_package(pkg)
+    message <<~EOM
+      pkg-config could not be used to find #{pkg}
+      Please install either `pkg-config` or the pkg-config gem per
 
-    cflags  = PKGConfig.cflags(pkg)
-    ldflags = PKGConfig.libs_only_L(pkg)
-    libs    = PKGConfig.libs_only_l(pkg)
+          gem install pkg-config -v #{gem_ver.inspect}
 
-    Logging::message "PKGConfig package configuration for %s\n", pkg
-    Logging::message "cflags: %s\nldflags: %s\nlibs: %s\n\n", cflags, ldflags, libs
-
-    [cflags, ldflags, libs]
+    EOM
+    return nil
   end
+
+  return nil unless PKGConfig.have_package(pkg)
+
+  cflags  = PKGConfig.cflags(pkg)
+  ldflags = PKGConfig.libs_only_L(pkg)
+  libs    = PKGConfig.libs_only_l(pkg)
+
+  Logging::message "PKGConfig package configuration for %s\n", pkg
+  Logging::message "cflags: %s\nldflags: %s\nlibs: %s\n\n", cflags, ldflags, libs
+
+  [cflags, ldflags, libs]
 end
 
 def have_libxml_headers?(version=nil)
