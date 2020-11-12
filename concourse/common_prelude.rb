32571e054a4c8bf1e787006436aa9b409dd7af32
Mike Dalessio <mike.dalessio@gmail.com>
2020-11-12 02:44:52 -0500

    ci: convert PR pipeline to a YTT template
    and we no longer put to Gitter or IRC from the PR pipeline, let's
    rely on the github PR checks.

--- concourse/common_prelude.rb
+++ concourse/common_prelude.rb
@@ -8,13 +8,5 @@ $git_resource_ignore_paths = [
   "Vagrantfile",
 ].to_json
 
-$git_pr_resource_ignore_paths = [
-  "*.md",
-  "concourse/",
-  "suppressions/",
-  ".github/",
-  "Vagrantfile",
-].to_json
-
 cross_rubies_path = File.join(File.dirname(__FILE__), "..", ".cross_rubies")
 $native_ruby_versions = File.read(cross_rubies_path).split("\n").map { |line| line.split(":").first.split(".").take(2).join(".") }.uniq.sort
