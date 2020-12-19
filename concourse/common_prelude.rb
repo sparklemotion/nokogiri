bb0ae65fc172fa1b02a8fda7f4074b6d8743f641
Mike Dalessio <mike.dalessio@gmail.com>
2020-12-19 15:10:45 -0500

    ci: convert nokogiri.yml to use ytt
    and break out rubocop as a separate job. the structure is now
    identical cross the master and PR pipelines.

--- concourse/common_prelude.rb
+++ concourse/common_prelude.rb
@@ -1,12 +1,6 @@
 require "json"
 
-$git_resource_ignore_paths = [
-  "*.md",
-  "concourse/**",
-  "suppressions/**",
-  ".github/**",
-  "Vagrantfile",
-].to_json
-
 cross_rubies_path = File.join(File.dirname(__FILE__), "..", ".cross_rubies")
-$native_ruby_versions = File.read(cross_rubies_path).split("\n").map { |line| line.split(":").first.split(".").take(2).join(".") }.uniq.sort
+$native_ruby_versions = File.read(cross_rubies_path).split("\n").map do |line|
+  line.split(":").first.split(".").take(2).join(".")
+end.uniq.sort
