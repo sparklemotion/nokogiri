c60b6eb9a27436237699eba392b248e00fd1e0dd
Mike Dalessio <mike.dalessio@gmail.com>
2020-12-27 11:17:34 -0500

    style: format ext/nokogiri/html_document.c

--- ext/nokogiri/html_document.c
+++ ext/nokogiri/html_document.c
@@ -8,19 +8,20 @@ static ID id_encoding_found;
  *
  * Create a new document
  */
-static VALUE new(int argc, VALUE *argv, VALUE klass)
+static VALUE
+new (int argc, VALUE *argv, VALUE klass)
 {
   VALUE uri, external_id, rest, rb_doc;
   htmlDocPtr doc;
 
   rb_scan_args(argc, argv, "0*", &rest);
-  uri         = rb_ary_entry(rest, (long)0);
+  uri = rb_ary_entry(rest, (long)0);
   external_id = rb_ary_entry(rest, (long)1);
 
   doc = htmlNewDoc(
-      RTEST(uri) ? (const xmlChar *)StringValueCStr(uri) : NULL,
-      RTEST(external_id) ? (const xmlChar *)StringValueCStr(external_id) : NULL
-  );
+          RTEST(uri) ? (const xmlChar *)StringValueCStr(uri) : NULL,
+          RTEST(external_id) ? (const xmlChar *)StringValueCStr(external_id) : NULL
+        );
   rb_doc = Nokogiri_wrap_xml_document(klass, doc);
   rb_obj_call_init(rb_doc, argc, argv);
   return rb_doc ;
@@ -33,15 +34,16 @@ static VALUE new(int argc, VALUE *argv, VALUE klass)
  * Read the HTML document from +io+ with given +url+, +encoding+,
  * and +options+.  See Nokogiri::HTML.parse
  */
-static VALUE read_io( VALUE klass,
-                      VALUE io,
-                      VALUE url,
-                      VALUE encoding,
-                      VALUE options )
+static VALUE
+read_io(VALUE klass,
+        VALUE io,
+        VALUE url,
+        VALUE encoding,
+        VALUE options)
 {
-  const char * c_url    = NIL_P(url)      ? NULL : StringValueCStr(url);
-  const char * c_enc    = NIL_P(encoding) ? NULL : StringValueCStr(encoding);
-  VALUE error_list      = rb_ary_new();
+  const char *c_url = NIL_P(url)      ? NULL : StringValueCStr(url);
+  const char *c_enc = NIL_P(encoding) ? NULL : StringValueCStr(encoding);
+  VALUE error_list = rb_ary_new();
   VALUE document;
   htmlDocPtr doc;
 
@@ -49,13 +51,13 @@ static VALUE read_io( VALUE klass,
   xmlSetStructuredErrorFunc((void *)error_list, Nokogiri_error_array_pusher);
 
   doc = htmlReadIO(
-      io_read_callback,
-      io_close_callback,
-      (void *)io,
-      c_url,
-      c_enc,
-      (int)NUM2INT(options)
-  );
+          io_read_callback,
+          io_close_callback,
+          (void *)io,
+          c_url,
+          c_enc,
+          (int)NUM2INT(options)
+        );
   xmlSetStructuredErrorFunc(NULL, NULL);
 
   /*
@@ -70,16 +72,17 @@ static VALUE read_io( VALUE klass,
     }
   }
 
-  if(doc == NULL) {
+  if (doc == NULL) {
     xmlErrorPtr error;
 
     xmlFreeDoc(doc);
 
     error = xmlGetLastError();
-    if(error)
+    if (error) {
       rb_exc_raise(Nokogiri_wrap_xml_syntax_error(error));
-    else
+    } else {
       rb_raise(rb_eRuntimeError, "Could not parse document");
+    }
 
     return Qnil;
   }
@@ -96,17 +99,18 @@ static VALUE read_io( VALUE klass,
  * Read the HTML document contained in +string+ with given +url+, +encoding+,
  * and +options+.  See Nokogiri::HTML.parse
  */
-static VALUE read_memory( VALUE klass,
-                          VALUE string,
-                          VALUE url,
-                          VALUE encoding,
-                          VALUE options )
+static VALUE
+read_memory(VALUE klass,
+            VALUE string,
+            VALUE url,
+            VALUE encoding,
+            VALUE options)
 {
-  const char * c_buffer = StringValuePtr(string);
-  const char * c_url    = NIL_P(url)      ? NULL : StringValueCStr(url);
-  const char * c_enc    = NIL_P(encoding) ? NULL : StringValueCStr(encoding);
-  int len               = (int)RSTRING_LEN(string);
-  VALUE error_list      = rb_ary_new();
+  const char *c_buffer = StringValuePtr(string);
+  const char *c_url = NIL_P(url)      ? NULL : StringValueCStr(url);
+  const char *c_enc = NIL_P(encoding) ? NULL : StringValueCStr(encoding);
+  int len = (int)RSTRING_LEN(string);
+  VALUE error_list = rb_ary_new();
   VALUE document;
   htmlDocPtr doc;
 
@@ -116,16 +120,17 @@ static VALUE read_memory( VALUE klass,
   doc = htmlReadMemory(c_buffer, len, c_url, c_enc, (int)NUM2INT(options));
   xmlSetStructuredErrorFunc(NULL, NULL);
 
-  if(doc == NULL) {
+  if (doc == NULL) {
     xmlErrorPtr error;
 
     xmlFreeDoc(doc);
 
     error = xmlGetLastError();
-    if(error)
+    if (error) {
       rb_exc_raise(Nokogiri_wrap_xml_syntax_error(error));
-    else
+    } else {
       rb_raise(rb_eRuntimeError, "Could not parse document");
+    }
 
     return Qnil;
   }
@@ -141,7 +146,8 @@ static VALUE read_memory( VALUE klass,
  *
  * The type for this document
  */
-static VALUE type(VALUE self)
+static VALUE
+type(VALUE self)
 {
   htmlDocPtr doc;
   Data_Get_Struct(self, xmlDoc, doc);
@@ -149,14 +155,16 @@ static VALUE type(VALUE self)
 }
 
 VALUE cNokogiriHtmlDocument ;
-void init_html_document()
+
+void
+init_html_document()
 {
-  VALUE nokogiri  = rb_define_module("Nokogiri");
-  VALUE html      = rb_define_module_under(nokogiri, "HTML");
-  VALUE xml       = rb_define_module_under(nokogiri, "XML");
-  VALUE node      = rb_define_class_under(xml, "Node", rb_cObject);
-  VALUE xml_doc   = rb_define_class_under(xml, "Document", node);
-  VALUE klass     = rb_define_class_under(html, "Document", xml_doc);
+  VALUE nokogiri = rb_define_module("Nokogiri");
+  VALUE html = rb_define_module_under(nokogiri, "HTML");
+  VALUE xml = rb_define_module_under(nokogiri, "XML");
+  VALUE node = rb_define_class_under(xml, "Node", rb_cObject);
+  VALUE xml_doc = rb_define_class_under(xml, "Document", node);
+  VALUE klass = rb_define_class_under(html, "Document", xml_doc);
 
   cNokogiriHtmlDocument = klass;
 
