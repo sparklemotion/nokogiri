2c9f3995c184e908550b4d9848ac7c16d91abf77
Mike Dalessio <mike.dalessio@gmail.com>
2020-12-28 15:38:03 -0500

    ci: remove gem packaging commit logic

--- concourse/tasks/gem-test/gem-install-and-test.sh
+++ concourse/tasks/gem-test/gem-install-and-test.sh
@@ -4,14 +4,6 @@ test -e /etc/os-release && cat /etc/os-release
 
 set -e -x -u
 
-GEM_PACKAGING_REF=813d119
-function commit-is-post-gem-packaging-cleanup {
-  if git merge-base --is-ancestor ${GEM_PACKAGING_REF} HEAD ; then
-    return 0
-  fi
-  return 1
-}
-
 pushd gems
 
   gemfile=$(ls *.gem | head -n1)
@@ -38,10 +30,7 @@ pushd nokogiri
   rm -rf lib ext # ensure we can't use the local files
   bundle exec bash run-test
 
-  if commit-is-post-gem-packaging-cleanup ; then
-    if [[ -e ./scripts/test-gem-installation ]] ; then
-      gem install minitest-reporters # TODO: remove once PRs based on pre-1e57386 have passed
-      ./scripts/test-gem-installation
-    fi
+  if [[ -e ./scripts/test-gem-installation ]] ; then
+    ./scripts/test-gem-installation
   fi
 popd
