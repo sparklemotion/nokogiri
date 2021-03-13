b88324b8971aaebf9adfc4ea34e25bd136fc0626
Mike Dalessio <mike.dalessio@gmail.com>
2021-03-13 12:25:36 -0500

    license: update MIT license copyright dates
    and remove the out-of-date MIT license declaration from the java files
    in which it was reproduced.

--- ext/java/nokogiri/internals/dom2dtm/DOM2DTMdefaultNamespaceDeclarationNode.java
+++ ext/java/nokogiri/internals/dom2dtm/DOM2DTMdefaultNamespaceDeclarationNode.java
@@ -1,8 +1,4 @@
 /*
- * reserved comment block
- * DO NOT REMOVE OR ALTER!
- */
-/*
  * Copyright 1999-2004 The Apache Software Foundation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
