ac0c70f17ba1b23905434ba06da19de25f9cebdd
Mike Dalessio <mike.dalessio@gmail.com>
2022-12-14 09:37:03 -0500

    style(rubocop): Style/RedundantConstantBase

--- test/test_nokogumbo_contract.rb
+++ test/test_nokogumbo_contract.rb
@@ -7,22 +7,22 @@ describe "Nokogumbo contract expectations" do
   it "includes the HTML5 public interface" do
     skip("Gumbo is not supported on this platform") unless Nokogiri.uses_gumbo?
 
-    assert_includes(::Nokogiri.singleton_methods, :HTML5)
+    assert_includes(Nokogiri.singleton_methods, :HTML5)
 
-    assert_equal("constant", defined?(::Nokogiri::HTML5))
-    assert_includes(::Nokogiri::HTML5.singleton_methods, :parse)
-    assert_includes(::Nokogiri::HTML5.singleton_methods, :fragment)
+    assert_equal("constant", defined?(Nokogiri::HTML5))
+    assert_includes(Nokogiri::HTML5.singleton_methods, :parse)
+    assert_includes(Nokogiri::HTML5.singleton_methods, :fragment)
 
-    assert_equal("constant", defined?(::Nokogiri::HTML5::Node))
-    assert_equal("constant", defined?(::Nokogiri::HTML5::Document))
-    assert_equal("constant", defined?(::Nokogiri::HTML5::DocumentFragment))
+    assert_equal("constant", defined?(Nokogiri::HTML5::Node))
+    assert_equal("constant", defined?(Nokogiri::HTML5::Document))
+    assert_equal("constant", defined?(Nokogiri::HTML5::DocumentFragment))
   end
 
   it "includes a replacement for the Nokogumbo private interface" do
     skip("Gumbo is not supported on this platform") unless Nokogiri.uses_gumbo?
 
-    assert_equal("constant", defined?(::Nokogiri::Gumbo))
-    assert_includes(::Nokogiri::Gumbo.singleton_methods, :parse)
-    assert_includes(::Nokogiri::Gumbo.singleton_methods, :fragment)
+    assert_equal("constant", defined?(Nokogiri::Gumbo))
+    assert_includes(Nokogiri::Gumbo.singleton_methods, :parse)
+    assert_includes(Nokogiri::Gumbo.singleton_methods, :fragment)
   end
 end
