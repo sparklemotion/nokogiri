c247dad9f784fc333d7c64a2095cba58226b8c2f
Mike Dalessio <mike.dalessio@gmail.com>
2023-01-16 11:20:54 -0500

    test: update push parser tests to minispec
    and rewrite the calls causing rubocop errors

--- test/html4/sax/test_push_parser.rb
+++ test/html4/sax/test_push_parser.rb
@@ -3,74 +3,65 @@
 
 require "helper"
 
-module Nokogiri
-  module HTML
-    module SAX
-      class TestPushParser < Nokogiri::SAX::TestCase
-        def setup
-          super
-          @parser = Nokogiri::HTML4::SAX::PushParser.new(Doc.new)
-        end
+describe Nokogiri::HTML4::SAX::PushParser do
+  let(:parser) { Nokogiri::HTML4::SAX::PushParser.new(Nokogiri::SAX::TestCase::Doc.new) }
 
-        def test_end_document_called
-          @parser.<<(<<~eoxml)
-            <p id="asdfasdf">
-              <!-- This is a comment -->
-              Paragraph 1
-            </p>
-          eoxml
-          refute(@parser.document.end_document_called)
-          @parser.finish
-          assert(@parser.document.end_document_called)
-        end
+  it :test_end_document_called do
+    parser << (<<~HTML)
+      <p id="asdfasdf">
+        <!-- This is a comment -->
+        Paragraph 1
+      </p>
+    HTML
+    refute(parser.document.end_document_called)
+    parser.finish
+    assert(parser.document.end_document_called)
+  end
 
-        def test_start_element
-          @parser.<<(<<~eoxml)
-            <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-            <html><head><body><p id="asdfasdf">
-          eoxml
+  it :test_start_element do
+    parser << (<<~HTML)
+      <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+      <html><head><body><p id="asdfasdf">
+    HTML
 
-          assert_equal([["html", []], ["head", []], ["body", []], ["p", [["id", "asdfasdf"]]]],
-            @parser.document.start_elements)
+    assert_equal([["html", []], ["head", []], ["body", []], ["p", [["id", "asdfasdf"]]]],
+      parser.document.start_elements)
 
-          @parser.<<(<<~eoxml)
-            <!-- This is a comment -->
-            Paragraph 1
-            </p></body></html>
-          eoxml
-          assert_equal([" This is a comment "], @parser.document.comments)
-          @parser.finish
-        end
+    parser << (<<~HTML)
+      <!-- This is a comment -->
+      Paragraph 1
+      </p></body></html>
+    HTML
+    assert_equal([" This is a comment "], parser.document.comments)
+    parser.finish
+  end
 
-        def test_chevron_partial_html
-          @parser.<<(<<~eoxml)
-            <p id="asdfasdf">
-          eoxml
+  it :test_chevron_partial_html do
+    parser << (<<~HTML)
+      <p id="asdfasdf">
+    HTML
 
-          @parser.<<(<<-eoxml)
-              <!-- This is a comment -->
-              Paragraph 1
-            </p>
-          eoxml
-          assert_equal([" This is a comment "], @parser.document.comments)
-          @parser.finish
-        end
+    parser << (<<-HTML)
+        <!-- This is a comment -->
+        Paragraph 1
+      </p>
+    HTML
+    assert_equal([" This is a comment "], parser.document.comments)
+    parser.finish
+  end
 
-        def test_chevron
-          @parser.<<(<<~eoxml)
-            <p id="asdfasdf">
-              <!-- This is a comment -->
-              Paragraph 1
-            </p>
-          eoxml
-          @parser.finish
-          assert_equal([" This is a comment "], @parser.document.comments)
-        end
+  it :test_chevron do
+    parser << (<<~HTML)
+      <p id="asdfasdf">
+        <!-- This is a comment -->
+        Paragraph 1
+      </p>
+    HTML
+    parser.finish
+    assert_equal([" This is a comment "], parser.document.comments)
+  end
 
-        def test_default_options
-          assert_equal(0, @parser.options)
-        end
-      end
-    end
+  it :test_default_options do
+    assert_equal(0, parser.options)
   end
 end
