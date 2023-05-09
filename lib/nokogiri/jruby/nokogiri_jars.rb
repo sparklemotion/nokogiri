84edfa5e27603980ad09538d5172f51b9382ce59
Mike Dalessio <mike.dalessio@gmail.com>
2023-05-08 22:05:56 -0400

    dep: update jar files for xalan

--- lib/nokogiri/jruby/nokogiri_jars.rb
+++ lib/nokogiri/jruby/nokogiri_jars.rb
@@ -2,26 +2,26 @@
 begin
   require 'jar_dependencies'
 rescue LoadError
-  require 'xalan/xalan/2.7.3/xalan-2.7.3.jar'
+  require 'xalan/serializer/2.7.3/serializer-2.7.3.jar'
   require 'net/sourceforge/htmlunit/neko-htmlunit/2.63.0/neko-htmlunit-2.63.0.jar'
   require 'nu/validator/jing/20200702VNU/jing-20200702VNU.jar'
   require 'xerces/xercesImpl/2.12.2/xercesImpl-2.12.2.jar'
   require 'org/nokogiri/nekodtd/0.1.11.noko1/nekodtd-0.1.11.noko1.jar'
   require 'net/sf/saxon/Saxon-HE/9.6.0-4/Saxon-HE-9.6.0-4.jar'
+  require 'xalan/xalan/2.7.3/xalan-2.7.3.jar'
   require 'xml-apis/xml-apis/1.4.01/xml-apis-1.4.01.jar'
-  require 'xalan/serializer/2.7.3/serializer-2.7.3.jar'
   require 'isorelax/isorelax/20030108/isorelax-20030108.jar'
 end
 
 if defined? Jars
-  require_jar 'xalan', 'xalan', '2.7.3'
+  require_jar 'xalan', 'serializer', '2.7.3'
   require_jar 'net.sourceforge.htmlunit', 'neko-htmlunit', '2.63.0'
   require_jar 'nu.validator', 'jing', '20200702VNU'
   require_jar 'xerces', 'xercesImpl', '2.12.2'
   require_jar 'org.nokogiri', 'nekodtd', '0.1.11.noko1'
   require_jar 'net.sf.saxon', 'Saxon-HE', '9.6.0-4'
+  require_jar 'xalan', 'xalan', '2.7.3'
   require_jar 'xml-apis', 'xml-apis', '1.4.01'
-  require_jar 'xalan', 'serializer', '2.7.3'
   require_jar 'isorelax', 'isorelax', '20030108'
 end
 
