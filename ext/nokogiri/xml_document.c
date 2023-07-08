14d1f5a2a11e2cdad540ee33b05985e4301e7b4b
Mike Dalessio <mike.dalessio@gmail.com>
2023-07-08 09:47:59 -0400

    test: add coverage for the memsize_of bug
    which turns out to be because xmlDTD has "attributes" where xmlNode
    has "properties"
    
    (cherry picked from commit 19a64ba54f7673729ebae4d572711259223fbce8)

--- ext/nokogiri/xml_document.c
+++ ext/nokogiri/xml_document.c
@@ -100,13 +100,14 @@ memsize_node(const xmlNodePtr node)
 {
   /* note we don't count namespace definitions, just going for a good-enough number here */
   xmlNodePtr child;
+  xmlAttrPtr property;
   size_t memsize = 0;
 
   memsize += xmlStrlen(node->name);
 
   if (node->type == XML_ELEMENT_NODE) {
-    for (child = (xmlNodePtr)node->properties; child; child = child->next) {
-      memsize += sizeof(xmlAttr) + memsize_node(child);
+    for (property = node->properties; property; property = property->next) {
+      memsize += sizeof(xmlAttr) + memsize_node((xmlNodePtr)property);
     }
   }
   if (node->type == XML_TEXT_NODE) {
