a9588dd5e16139974590619740c3a68a3b6ff885
Mike Dalessio <mike.dalessio@gmail.com>
2023-11-20 15:32:25 -0500

    dev(fuzz): convert fuzzer/build.sh to a bash script
    and
    - set some bash safety flags
    - make it runnable from any directory
    - simplify the makefile execution

--- gumbo-parser/fuzzer/build.sh
+++ gumbo-parser/fuzzer/build.sh
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+cd $(dirname $0)
+
 export SANITIZER_OPTS=""
 export SANITIZER_LINK=""
 
-if [ -z "${LLVM_CONFIG}" ]
-then
-  if [ -x "$(command -v llvm-config)" ]; then
+if [[ -z "${LLVM_CONFIG:-}" ]] ; then
+  if [[ -x "$(command -v llvm-config)" ]]; then
     LLVM_CONFIG=$(which llvm-config)
   else
     echo 'llvm-config could not be found and $LLVM_CONFIG has not been set, expecting "export LLVM_CONFIG=/usr/bin/llvm-config-12" assuming clang-12 is installed, however any clang version works'
@@ -11,10 +16,7 @@ then
   fi
 fi
 
-if [ ! -d "build" ]
-then
-  mkdir build
-fi
+mkdir -p build
 
 export CC="$($LLVM_CONFIG --bindir)/clang"
 export CXX="$($LLVM_CONFIG --bindir)/clang++"
@@ -22,18 +24,15 @@ export CXXFLAGS="-fsanitize=fuzzer-no-link"
 export CFLAGS="-fsanitize=fuzzer-no-link"
 export ENGINE_LINK="$(find $($LLVM_CONFIG --libdir) -name libclang_rt.fuzzer-x86_64.a | head -1)"
 
-if [ "$SANITIZER" = "undefined" ]
-then
+if [[ "${SANITIZER:-}" = "undefined" ]] ; then
   export SANITIZER_OPTS="-fsanitize=undefined"
   export SANITIZER_LINK="$(find $($LLVM_CONFIG --libdir) -name libclang_rt.ubsan_standalone_cxx-x86_64.a | head -1)"
 fi
-if [ "$SANITIZER" = "address" ]
-then
+if [[ "${SANITIZER:-}" = "address" ]] ; then
   export SANITIZER_OPTS="-fsanitize=address"
   export SANITIZER_LINK="$(find $($LLVM_CONFIG --libdir) -name libclang_rt.asan_cxx-x86_64.a | head -1)"
 fi
-if [ "$SANITIZER" = "memory" ]
-then
+if [[ "${SANITIZER:-}" = "memory" ]] ; then
   export SANITIZER_OPTS="-fsanitize=memory -fPIE -pie -Wno-unused-command-line-argument"
   export SANITIZER_LINK="$(find $($LLVM_CONFIG --libdir) -name libclang_rt.msan_cxx-x86_64.a | head -1)"
 fi
@@ -42,8 +41,7 @@ export CXXFLAGS="-O3 $CXXFLAGS $SANITIZER_OPTS"
 export CFLAGS="-O3 $CFLAGS $SANITIZER_OPTS"
 cd ../src && make clean && make && cd -
 
-if [ -z "${SANITIZER}" ]
-then
+if [[ -z "${SANITIZER:-}" ]] ; then
   $CXX $CXXFLAGS -o build/parse_fuzzer parse_fuzzer.cc ../src/libgumbo.a $ENGINE_LINK $SANITIZER_LINK
 else
   $CXX $CXXFLAGS -o build/parse_fuzzer-$SANITIZER parse_fuzzer.cc ../src/libgumbo.a $ENGINE_LINK $SANITIZER_LINK
