01a9dac0197682b9c14f26bc7d92034b431f03eb
fuzzy-boiii23a <fuzzy.boiiii23a@gmail.com>
2024-01-13 13:00:23 +1000

    maintain oss-fuzz target building upstream

---
+++ gumbo-parser/fuzzer/build-ossfuzz.sh
@@ -0,0 +1,7 @@
+cd src
+make
+cd ../
+
+$CXX $CXXFLAGS -o $OUT/parse_fuzzer fuzzer/parse_fuzzer.cc src/libgumbo.a $LIB_FUZZING_ENGINE
+cp fuzzer/gumbo.dict $OUT/parse_fuzzer.dict
+cp fuzzer/gumbo_corpus.zip $OUT/parse_fuzzer_seed_corpus.zip
