e54cd33882f5fd210029a8355b8c478fb27ac235
Mike Dalessio <mike.dalessio@gmail.com>
2024-03-22 10:21:56 -0400

    test: update tests to reflect upstream libxml2 text node coalescing
    A text node coalescing bug was fixed in gnome/libxml2@4ccd3eb8, and
    although Nokogiri handles this fine from a memory perspective, the
    tests need to be updated to reflect the new behavior.

--- test/xml/test_node_reparenting.rb
+++ test/xml/test_node_reparenting.rb
@@ -623,7 +623,11 @@ module Nokogiri
               assert_equal "after", after.content
               refute_nil after.parent, "unrelated node should not be affected"
 
-              assert_equal "before", before.content
+              if Nokogiri.uses_libxml?(">= 2.13.0")
+                assert_equal "beforex", before.content # coalescing fixed in gnome/libxml2@4ccd3eb8
+              else
+                assert_equal "before", before.content
+              end
               refute_nil before.parent, "no need to reparent"
             end
           end
@@ -662,7 +666,11 @@ module Nokogiri
               assert_equal "before", before.content
               refute_nil before.parent, "unrelated node should not be affected"
 
-              assert_equal "after", after.content
+              if Nokogiri.uses_libxml?(">= 2.13.0")
+                assert_equal "xafter", after.content # coalescing fixed in gnome/libxml2@4ccd3eb8
+              else
+                assert_equal "after", after.content
+              end
               refute_nil after.parent
             end
           end
