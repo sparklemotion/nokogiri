28680f5b1327c71d51b9e3ca247eb31365b7bba5
Mike Dalessio <mike.dalessio@gmail.com>
2024-07-07 01:40:23 -0400

    tidy: update test/*/sax/test_parser_context.rb to use minitest::spec

--- test/html4/sax/test_parser_context.rb
+++ test/html4/sax/test_parser_context.rb
@@ -3,52 +3,53 @@
 
 require "helper"
 
-module Nokogiri
-  module HTML
-    module SAX
-      class TestParserContext < Nokogiri::SAX::TestCase
-        def test_from_io
+module Nokogiri::HTML4::SAX
+  describe Nokogiri::HTML4::SAX::ParserContext do
+    describe "constructor" do
+      describe ".new" do
+        it "handles IO" do
           ctx = ParserContext.new(StringIO.new("fo"), "UTF-8")
           assert(ctx)
         end
 
-        def test_from_string
+        it "handles String" do
           ctx = ParserContext.new("blah blah")
           assert(ctx)
         end
+      end
 
-        def test_parse_with
-          ctx = ParserContext.new("blah")
-          assert_raises(ArgumentError) do
-            ctx.parse_with(nil)
-          end
-        end
+      it ".file" do
+        ctx = ParserContext.file(Nokogiri::TestCase::HTML_FILE, "UTF-8")
+        parser = Parser.new(Nokogiri::SAX::TestCase::Doc.new)
+        ctx.parse_with(parser)
 
-        def test_parse_with_sax_parser
-          refute_raises do
-            xml = "<root />"
-            ctx = ParserContext.new(xml)
-            parser = Parser.new(Doc.new)
-            ctx.parse_with(parser)
-          end
-        end
+        assert(parser.document.start_document_called)
+        assert(parser.document.end_document_called)
+      end
 
-        def test_from_file
-          refute_raises do
-            ctx = ParserContext.file(HTML_FILE, "UTF-8")
-            parser = Parser.new(Doc.new)
-            ctx.parse_with(parser)
-          end
-        end
+      it "gracefully handles invalid types" do
+        assert_raises(TypeError) { ParserContext.new(0xcafecafe) }
+        assert_raises(TypeError) { ParserContext.memory(0xcafecafe) }
+        assert_raises(TypeError) { ParserContext.io(0xcafecafe) }
+        assert_raises(TypeError) { ParserContext.file(0xcafecafe) }
+      end
+    end
 
-        def test_graceful_handling_of_invalid_types
-          assert_raises(TypeError) { ParserContext.new(0xcafecafe) }
-          assert_raises(TypeError) { ParserContext.memory(0xcafecafe, "UTF-8") }
-          assert_raises(TypeError) { ParserContext.io(0xcafecafe, 1) }
-          assert_raises(TypeError) { ParserContext.io(StringIO.new("asdf"), "should be an index into ENCODINGS") }
-          assert_raises(TypeError) { ParserContext.file(0xcafecafe, "UTF-8") }
-          assert_raises(TypeError) { ParserContext.file("path/to/file", 0xcafecafe) }
+    describe "#parse_with" do
+      it "raises when passed nil" do
+        ctx = ParserContext.new("blah")
+        assert_raises(ArgumentError) do
+          ctx.parse_with(nil)
         end
+      end
+
+      it "parses when passed a sax parser" do
+        ctx = ParserContext.new("<root/>")
+        parser = Parser.new(Nokogiri::SAX::TestCase::Doc.new)
+
+        assert_nil(ctx.parse_with(parser))
+        assert(parser.document.start_document_called)
+        assert(parser.document.end_document_called)
       end
     end
   end
