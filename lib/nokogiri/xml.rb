9435520197fea177ac95dd674f3e918da8c9ca2f
Brian Lesperance <github@brianlesperance.com>
2024-11-14 15:24:39 -0600

    Nokogiri::XML.parse() supports argument forwarding
    Related to https://github.com/sparklemotion/nokogiri#3323

--- lib/nokogiri/xml.rb
+++ lib/nokogiri/xml.rb
@@ -2,8 +2,7 @@
 
 module Nokogiri
   class << self
-    ###
-    # Parse XML.  Convenience method for Nokogiri::XML::Document.parse
+    # Convenience method for Nokogiri::XML::Document.parse
     def XML(...)
       Nokogiri::XML::Document.parse(...)
     end
@@ -23,10 +22,9 @@ module Nokogiri
         Reader.new(...)
       end
 
-      ###
-      # Parse XML.  Convenience method for Nokogiri::XML::Document.parse
-      def parse(thing, url = nil, encoding = nil, options = ParseOptions::DEFAULT_XML, &block)
-        Document.parse(thing, url, encoding, options, &block)
+      # Convenience method for Nokogiri::XML::Document.parse
+      def parse(...)
+        Document.parse(...)
       end
 
       ####
--- test/xml/test_document.rb
+++ test/xml/test_document.rb
@@ -1101,6 +1101,13 @@ module Nokogiri
                 end
                 assert_nil(error.path)
               end
+
+              it "raises exception on parse error with kwargs" do
+                error = assert_raises Nokogiri::SyntaxError do
+                  Nokogiri::XML.parse(input, options: parse_options)
+                end
+                assert_nil(error.path)
+              end
             end
 
             describe "default options" do
@@ -1120,6 +1127,12 @@ module Nokogiri
               it "raises exception on parse error" do
                 assert_raises Nokogiri::SyntaxError do
                   Nokogiri::XML.parse(input, nil, nil, parse_options)
+                end
+              end
+
+              it "raises exception on parse error with kwargs" do
+                assert_raises Nokogiri::SyntaxError do
+                  Nokogiri::XML.parse(input, options: parse_options)
                 end
               end
             end
