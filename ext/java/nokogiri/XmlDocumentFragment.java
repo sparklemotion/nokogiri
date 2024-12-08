7b505527ab7e73a949c63aec0fb855483b099906
Mike Dalessio <mike.dalessio@gmail.com>
2024-12-08 10:43:58 -0500

    Extract JRuby DocumentFragment initializer parameter handling
    and delete some unused code.

--- ext/java/nokogiri/XmlDocumentFragment.java
+++ ext/java/nokogiri/XmlDocumentFragment.java
@@ -48,110 +48,22 @@ public class XmlDocumentFragment extends XmlNode
     super(ruby, klazz);
   }
 
-  @JRubyMethod(name = "new", meta = true, required = 1, optional = 3)
+  @JRubyMethod(name = "native_new", meta = true)
   public static IRubyObject
-  rbNew(ThreadContext context, IRubyObject cls, IRubyObject[] args, Block block)
+  rbNew(ThreadContext context, IRubyObject cls, IRubyObject value)
   {
-    if (args.length < 1) {
-      throw context.runtime.newArgumentError(args.length, 1);
-    }
-
-    if (!(args[0] instanceof XmlDocument)) {
+    if (!(value instanceof XmlDocument)) {
       throw context.runtime.newArgumentError("first parameter must be a Nokogiri::XML::Document instance");
     }
 
-    XmlDocument doc = (XmlDocument) args[0];
-
-    // make well-formed fragment, ignore invalid namespace, or add appropriate namespace to parse
-    if (args.length > 1 && args[1] instanceof RubyString) {
-      final RubyString arg1 = (RubyString) args[1];
-      if (XmlDocumentFragment.isTag(arg1)) {
-        args[1] = RubyString.newString(context.runtime, addNamespaceDeclIfNeeded(doc, rubyStringToString(arg1)));
-      }
-    }
+    XmlDocument doc = (XmlDocument) value;
 
     XmlDocumentFragment fragment = (XmlDocumentFragment) NokogiriService.XML_DOCUMENT_FRAGMENT_ALLOCATOR.allocate(
                                      context.runtime, (RubyClass)cls);
     fragment.setDocument(context, doc);
     fragment.setNode(context.runtime, doc.getDocument().createDocumentFragment());
 
-    Helpers.invoke(context, fragment, "initialize", args, block);
     return fragment;
-  }
-
-  private static final ByteList TAG_BEG = ByteList.create("<");
-  private static final ByteList TAG_END = ByteList.create(">");
-
-  private static boolean
-  isTag(final RubyString str)
-  {
-    return str.getByteList().startsWith(TAG_BEG) && str.getByteList().endsWith(TAG_END);
-  }
-
-  private static boolean
-  isNamespaceDefined(String qName, NamedNodeMap nodeMap)
-  {
-    if (isNamespace(qName.intern())) { return true; }
-    for (int i = 0; i < nodeMap.getLength(); i++) {
-      Attr attr = (Attr)nodeMap.item(i);
-      if (isNamespace(attr.getNodeName())) {
-        String localPart = getLocalNameForNamespace(attr.getNodeName(), null);
-        if (getPrefix(qName).equals(localPart)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-  private static final Pattern QNAME_RE = Pattern.compile("[^</:>\\s]+:[^</:>=\\s]+");
-  private static final Pattern START_TAG_RE = Pattern.compile("<[^</>]+>");
-
-  private static String
-  addNamespaceDeclIfNeeded(XmlDocument doc, String tags)
-  {
-    if (doc.getDocument() == null) { return tags; }
-    if (doc.getDocument().getDocumentElement() == null) { return tags; }
-    Matcher matcher = START_TAG_RE.matcher(tags);
-    Map<CharSequence, CharSequence> rewriteTable = null;
-    while (matcher.find()) {
-      String start_tag = matcher.group();
-      Matcher matcher2 = QNAME_RE.matcher(start_tag);
-      while (matcher2.find()) {
-        String qName = matcher2.group();
-        NamedNodeMap nodeMap = doc.getDocument().getDocumentElement().getAttributes();
-        if (isNamespaceDefined(qName, nodeMap)) {
-          CharSequence namespaceDecl = getNamespaceDecl(getPrefix(qName), nodeMap);
-          if (namespaceDecl != null) {
-            if (rewriteTable == null) { rewriteTable = new HashMap<CharSequence, CharSequence>(8, 1); }
-            StringBuilder str = new StringBuilder(qName.length() + namespaceDecl.length() + 3);
-            String key = str.append('<').append(qName).append('>').toString();
-            str.setCharAt(key.length() - 1, ' '); // (last) '>' -> ' '
-            rewriteTable.put(key, str.append(namespaceDecl).append('>'));
-          }
-        }
-      }
-    }
-    if (rewriteTable != null) {
-      for (Map.Entry<CharSequence, CharSequence> e : rewriteTable.entrySet()) {
-        tags = tags.replace(e.getKey(), e.getValue());
-      }
-    }
-
-    return tags;
-  }
-
-  private static CharSequence
-  getNamespaceDecl(final String prefix, NamedNodeMap nodeMap)
-  {
-    for (int i = 0; i < nodeMap.getLength(); i++) {
-      Attr attr = (Attr) nodeMap.item(i);
-      if (prefix.equals(attr.getLocalName())) {
-        return new StringBuilder().
-               append(attr.getName()).append('=').append('"').append(attr.getValue()).append('"');
-      }
-    }
-    return null;
   }
 
   @Override
