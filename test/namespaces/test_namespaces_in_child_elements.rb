7c330a168470dc16e8e68ade3ef129965bad02cd
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-03-09 17:13:59 +0900

    Add failing tests

--- test/namespaces/test_namespaces_in_child_elements.rb
+++ test/namespaces/test_namespaces_in_child_elements.rb
@@ -76,10 +76,13 @@ describe 'namespaces in child elements' do
       assert_includes(doc, '<root xmlns="http://outer-namespace.org/">')
       assert_includes(doc, '<outer>in outer namespace</outer>')
       assert_includes(doc, '<inner xmlns="http://inner-namespace.org/">')
-      # TODO: THE NEXT LINE FAILS ON JRUBY!!
-      assert_includes(doc, '<element>in inner namespace</element>')
-      # TODO: THE NEXT LINE PASSES ON JRUBY, BUT SHOULD FAIL!!
-      # assert_includes(doc, '<element xmlns="http://outer-namespace.org/">in inner namespace</element>')
+      # TODO: JRuby's behavior here is incorrect.
+      # See: https://github.com/sparklemotion/nokogiri/issues/3457
+      if Nokogiri.jruby?
+        assert_includes(doc, '<element xmlns="http://outer-namespace.org/">in inner namespace</element>')
+      else
+        assert_includes(doc, '<element>in inner namespace</element>')
+      end
       assert_includes(doc, '<another>back in outer namespace</another>')
     end
   end
@@ -179,9 +182,13 @@ describe 'namespaces in child elements' do
       assert_includes(doc, '<default_element>in default namespace</default_element>')
       assert_includes(doc, '<ns:prefixed_element>in prefixed namespace</ns:prefixed_element>')
       assert_includes(doc, '<mixed xmlns="http://new-default.org/">')
-      # assert_includes(doc, '<new_default>in new default namespace</new_default>')
-      # TODO: THE NEXT LINE IS WRONG!!
-      assert_includes(doc, '<new_default xmlns="http://default.org/">in new default namespace</new_default>')
+      # TODO: JRuby's behavior here is incorrect.
+      # See: https://github.com/sparklemotion/nokogiri/issues/3457
+      if Nokogiri.jruby?
+        assert_includes(doc, '<new_default xmlns="http://default.org/">in new default namespace</new_default>')
+      else
+        assert_includes(doc, '<new_default>in new default namespace</new_default>')
+      end
       assert_includes(doc, '<ns:still_prefixed>still using original prefixed namespace</ns:still_prefixed>')
     end
   end
