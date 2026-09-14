sparklemotion/nokogiri

commit 26fb8e5bf4fdace3fcafaefd8dc1717055012fe8

cleaning up the attr method on NodeSet

lib/nokogiri/xml/node_set.rb
@@ -161,10 +161,9 @@ module Nokogiri
       # Remove the class attribute +name+ from all Node objects in the NodeSet.
       def remove_class name = nil
         each do |el|
-          next unless el.respond_to? :get_attribute
           if name
             classes = el.get_attribute('class').to_s.split(" ")
-            el.set_attribute('class', (classes - [name]).uniq.join(" "))
+            el['class'] = (classes - [name]).uniq.join(" ")
           else
             el.remove_attribute("class")
           end
@@ -176,20 +175,18 @@ module Nokogiri
       # Set the attribute +key+ to +value+ or the return value of +blk+
       # on all Node objects in the NodeSet.
       def attr key, value = nil, &blk
-        if value or blk
-          each do |el|
-            el.set_attribute(key, value || blk[el])
-          end
-          return self
+        unless Hash === key || key && (value || blk)
+          return first.attribute key
         end
-        if key.is_a? Hash
-          key.each { |k,v| self.attr(k,v) }
-          return self
-        else
-          return self[0].get_attribute(key)
-        end
+
+        hash = key.is_a?(Hash) ? key : { key => value }
+
+        hash.each { |k,v| each { |el| el[k] = v || blk[el] } }
+
+        self
       end
-      alias_method :set, :attr
+      alias :set :attr
+      alias :attribute :attr
 
       ###
       # Remove the attributed named +name+ from all Node objects in the NodeSet

test/xml/test_node_set.rb
@@ -8,6 +8,42 @@ module Nokogiri
         @xml = Nokogiri::XML(File.read(XML_FILE), XML_FILE)
       end
 
+      def test_attribute_set
+        list = @xml.css('employee')
+        list.each { |e| assert_nil e['foo'] }
+
+        [ ['attribute', 'bar'], ['attr', 'biz'], ['set', 'baz'] ].each do |t|
+          list.send(t.first.to_sym, 'foo', t.last)
+          list.each { |e| assert_equal t.last, e['foo'] }
+        end
+      end
+
+      def test_attribute_set_with_block
+        list = @xml.css('employee')
+        list.each { |e| assert_nil e['foo'] }
+
+        [ ['attribute', 'bar'], ['attr', 'biz'], ['set', 'baz'] ].each do |t|
+          list.send(t.first.to_sym, 'foo') { |x| t.last }
+          list.each { |e| assert_equal t.last, e['foo'] }
+        end
+      end
+
+      def test_attribute_set_with_hash
+        list = @xml.css('employee')
+        list.each { |e| assert_nil e['foo'] }
+
+        [ ['attribute', 'bar'], ['attr', 'biz'], ['set', 'baz'] ].each do |t|
+          list.send(t.first.to_sym, 'foo' => t.last)
+          list.each { |e| assert_equal t.last, e['foo'] }
+        end
+      end
+
+      def test_attribute_no_args
+        list = @xml.css('employee')
+        list.first['foo'] = 'bar'
+        assert_equal list.first.attribute('foo'), list.attribute('foo')
+      end
+
       def test_search_empty_node_set
         set = Nokogiri::XML::NodeSet.new(Nokogiri::XML::Document.new)
         assert_equal 0, set.css('foo').length

test/hpricot/test_alter.rb
@@ -36,16 +36,6 @@ class TestAlter < Nokogiri::TestCase
     assert !(Nokogiri(@basic.to_html)/"p:gt(1)")[0]["class"].split(" ").include?("testing123")
   end
 
-  def test_change_attributes
-    all_ps = (@basic/"p").attr("title", "Some Title")
-    all_as = (@basic/"a").attr("href", "http://my_new_href.com")
-    all_lb = (@basic/"link").attr("href") { |e| e.name }
-    GC.start # try to shake out GC bugs with xpath and node sets.
-    assert_changed(@basic, "p", all_ps) {|p| p.attributes["title"].to_s == "Some Title"}
-    assert_changed(@basic, "a", all_as) {|a| a.attributes["href"].to_s == "http://my_new_href.com"}
-    assert_changed(@basic, "link", all_lb) {|a| a.attributes["href"].to_s == "link" }
-  end
-
   def test_remove_attr
     all_rl = (@basic/"link").remove_attr("href")
     assert_changed(@basic, "link", all_rl) { |link| link['href'].nil? }

test/hpricot/test_parser.rb
@@ -4,13 +4,6 @@ require File.join(File.dirname(__FILE__),"load_files")
 class TestParser < Nokogiri::TestCase
   include Nokogiri
 
-  def test_set_attr
-    @basic = Nokogiri.parse(TestFiles::BASIC)
-    @basic.search('//p').set('class', 'para')
-    assert_equal 4, @basic.search('//p').length
-    assert_equal 4, @basic.search('//p').find_all { |x| x['class'] == 'para' }.length
-  end
-
   def test_filter_by_attr
     @boingboing = Nokogiri.parse(TestFiles::BOINGBOING)
 

commit c154fc17b00c7732fad0f7e2d1b60e8fdadab715

fix: NodeSet#attr dispatches on arity, not truthiness
`NodeSet#attr` used truthiness to decide whether a value was supplied,
so an explicit `false` or `nil` read as an omitted argument.
`attr("a", false)` returned a getter result without setting anything,
and `attr("a" => false)` raised `LocalJumpError`. The latter is a
regression from 26fb8e5b, which merged the positional and Hash branches
into `el[k] = v || blk[el]`.

Distinguish an omitted value with a private sentinel, and drop the block
fallback from the Hash branch. Raise `ArgumentError` when a value and a
block are both given; the docs already disallowed it and the old code
silently discarded the value.

`attr(key, nil)` now sets the empty string where it used to be a no-op
getter.

[Fix #3678]

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>

lib/nokogiri/xml/node_set.rb
@@ -15,6 +15,10 @@ module Nokogiri
       include Nokogiri::XML::Searchable
       include Enumerable
 
+      # Distinguishes an omitted argument from an explicit +false+ or +nil+.
+      NOT_GIVEN = Object.new.freeze
+      private_constant :NOT_GIVEN
+
       # The Document this NodeSet is associated with
       attr_accessor :document
 
@@ -184,10 +188,10 @@ module Nokogiri
       # called as a setter, +#attr+ returns the NodeSet.
       #
       # If +key+ is an attribute name, then either +value+ or +block+
-      # must be passed.
+      # must be passed. Passing both raises ArgumentError.
       #
       # If +key+ is a Hash then attributes will be set for each
-      # key/value pair:
+      # key/value pair, and passing a block raises ArgumentError:
       #
       #   node_set.attr("href" => "https://www.nokogiri.org", "class" => "member")
       #
@@ -196,23 +200,28 @@ module Nokogiri
       #
       #   node_set.attr("href", "https://www.nokogiri.org")
       #
+      # Values are coerced with +to_s+, the same as Node#[]=, so +false+ sets the string "false"
+      # and +nil+ sets the empty string. Use #remove_attr to remove an attribute.
+      #
       # If +block+ is passed, it will be called on each Node object in
       # the NodeSet and the return value used as the attribute value
       # for that node:
       #
       #   node_set.attr("class") { |node| node.name }
       #
-      def attr(key, value = nil, &block)
-        unless key.is_a?(Hash) || (key && (value || block))
-          return first&.attribute(key)
-        end
+      def attr(key, value = NOT_GIVEN, &block)
+        if key.is_a?(Hash)
+          raise(ArgumentError, "cannot pass both a Hash and a block") if block
 
-        hash = key.is_a?(Hash) ? key : { key => value }
+          key.each { |name, val| each { |node| node[name] = val } }
+        elsif NOT_GIVEN.equal?(value)
+          return first&.attribute(key) unless block
 
-        hash.each do |k, v|
-          each do |node|
-            node[k] = v || yield(node)
-          end
+          each { |node| node[key] = block.call(node) }
+        else
+          raise(ArgumentError, "cannot pass both a value and a block") if block
+
+          each { |node| node[key] = value }
         end
 
         self

test/xml/test_node_set.rb
@@ -126,6 +126,34 @@ module Nokogiri
               list.send(method, { "foo" => "bar" })
               list.each { |e| assert_equal("bar", e["foo"]) }
             end
+
+            it "sets a false attribute value" do
+              assert_same(list, list.send(method, "foo", false))
+              list.each { |e| assert_equal("false", e["foo"]) }
+            end
+
+            it "sets a nil attribute value" do
+              assert_same(list, list.send(method, "foo", nil))
+              list.each { |e| assert_equal("", e["foo"]) }
+            end
+
+            it "sets a false attribute value given a hash" do
+              assert_same(list, list.send(method, { "foo" => false }))
+              list.each { |e| assert_equal("false", e["foo"]) }
+            end
+
+            it "sets a nil attribute value given a hash" do
+              assert_same(list, list.send(method, { "foo" => nil }))
+              list.each { |e| assert_equal("", e["foo"]) }
+            end
+
+            it "raises when given both a value and a block" do
+              assert_raises(ArgumentError) { list.send(method, "foo", "bar") { "baz" } }
+            end
+
+            it "raises when given both a hash and a block" do
+              assert_raises(ArgumentError) { list.send(method, { "foo" => "bar" }) { "baz" } }
+            end
           end
         end
 